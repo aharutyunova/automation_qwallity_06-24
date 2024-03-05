1a5ac3a89bd6dfbfd8ae8862336c63e07fc70681
lianapan <liana.panosyan17@gmail.com>
2024-03-05 23:28:34 +0400

    First Program.

---
+++ Lesson_3_Liana_Panosyan/Liana_Panosyan.py
@@ -0,0 +1,14 @@
+# 1) Prints 'It's my first program’ and adds '!' at the end on the first line.
+print("It's my first program", end="!\n")
+# 2) Prints your name and surname on the second line.
+print("Liana Panosyan")
+# 3) Adds a comment about your background on the third line.
+
+'''I am a Manual QA Engineer at Softconstruct
+I studied at the Faculty of Informatics and Applied Mathematics of Yerevan
+State University.
+'''
+# 4) Sum two arbitrary numbers and print the result on the fourth line.
+number_1 = 71
+number_2 = 8
+print("Sum of number_1 and number_2 is", number_1+number_2)
