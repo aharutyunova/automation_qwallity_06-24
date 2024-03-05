7c9c8fb35e17523cf28a14d76d991fb5f4d10ccf
Arman Petrosyan <petrosyan.arman.a@gmail.com>
2024-03-05 20:20:22 +0400

    finished assignment of lesson 3

---
+++ Lesson_3/Arman.py
@@ -0,0 +1,19 @@
+# Lesson 3 assignment 
+
+# Prints 'It's my first program' and adds '!' at the end on the first line.
+print("It's my first program", end="!\n")
+
+# Prints your name and surname on the second line.
+my_name = "Arman"
+my_surname = "Petrosyan"
+print(f"My name is is {my_name} and surname is {my_surname}")
+
+# Adds a comment about your background on the third line.
+background_comment = "QA Engineer at VOLO"
+print(f"About my background {background_comment}")
+
+# Sum two arbitrary numbers and print the result on the fourth line
+number_one = 10
+number_two = 20
+sum_result = number_one + number_two
+print(f"Sum of two arbitrary numbers is {sum_result}")
