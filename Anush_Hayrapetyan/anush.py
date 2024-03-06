77749dfcdf945391925b8e2aab75118b5731d910
anna.harutyunova <anna.harutyunova@volo.global>
2024-03-06 18:46:41 +0400

    Add my comment

--- Anush_Hayrapetyan/anush.py
+++ Anush_Hayrapetyan/anush.py
@@ -2,3 +2,7 @@ print("It is my firts program", end='!')
 print("Anush Hayrapetyan")
 # I am a QA and work at Esterox
 print(15+25)
+
+
+# Anna comment - Everything ok only your Name and surname is not printed on the new line
+# In teh first print you should wite end='\n!'
