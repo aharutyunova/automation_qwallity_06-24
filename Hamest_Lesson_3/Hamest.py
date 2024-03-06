28db79392d325c3c7c12f1fcac39ad992dfcac14
hamestdolukhanyan <hamestdolukhanyan@gmail.com>
2024-03-06 16:55:37 +0400

    Hamest's first program

---
+++ Hamest_Lesson_3/Hamest.py
@@ -0,0 +1,12 @@
+'''This File is  about myself '''
+print("This is my first program", end="!\n")
+print("Hamest Dolukhanyan", end="\n\n")
+# I graduaded the State Engineerian Universyty of Armenia
+
+
+def hamestsummary(a, b):
+    # This function is about the summery of 2 numbers
+    print(a+b)
+
+
+hamestsummary(21, 12)
