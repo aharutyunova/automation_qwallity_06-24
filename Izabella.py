c6a5904355c4f22261cf8123b5008992f9935116
aharutyunova <55526198+aharutyunova@users.noreply.github.com>
2024-03-06 20:45:56 +0400

    Update Izabella.py

--- Izabella.py
+++ Izabella.py
@@ -5,3 +5,8 @@ print("Izabella Mnatsakanian")
 x = 1
 y = 29
 print(x + y)
+#Anna comment - everything is correct, 
+# you only don't print line 2 from the new line
+# To print from the new line you should 
+#write end='!\n'
+#Also no need was for merge)
