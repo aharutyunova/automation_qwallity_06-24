5eb3e685713792cf8e783981cc5c101c75f08365
JulietRev <julietrevazian@gmail.com>
2024-03-06 13:11:34 +0400

    task2

--- Anush_Hayrapetyan/anush.py
+++
@@ -1,4 +0,0 @@
-print("It is my firts program", end='!')
-print("Anush Hayrapetyan")
-# I am a QA and work at Esterox
-print(15+25)
---
+++ Lesson_3/juli.py
@@ -0,0 +1,4 @@
+print("It's my first program", end=("!\n"))
+print("Juliet Revazyan")
+#I'm a QA Engineer
+print(3 + 4)
