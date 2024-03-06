1c981b932358f3db352b31f7cb06182503915c72
anna.harutyunova <anna.harutyunova@volo.global>
2024-03-06 19:02:10 +0400

    Add my comment

--- Untitled-1.py
+++ Untitled-1.py
@@ -2,3 +2,9 @@ print("first lesson", end='!')
 print("Mariam Galoyan")
 # My home work
 print("1+4")
+
+# Anna comment - Mariam jan generally correct, I have 2 notes
+# 1.Your Name and surname is not printed on the new line
+# In the first print you should wite end='!\n'
+# 2.In line 4 for print sum you should not use quotes, in this case result is the string, for calculate to numbers sum you should print without quotes
+#  print(1+4)
