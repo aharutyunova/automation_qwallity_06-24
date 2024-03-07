8baeccfff960febb73f4e98072138ff3ea3d9a48
lianapan <liana.panosyan17@gmail.com>
2024-03-07 21:58:56 +0400

    Producing Fibonacci numbers using the Python programming language.

---
+++ Fibonacci folder/Fibonacci.py
@@ -0,0 +1,15 @@
+def Fibonacci(n):
+    first_member = 0
+    second_member = 1
+    if n <= 0:
+        print("Invalid n value")
+    else:
+        print(first_member, end=" ")
+        for i in range(1, n):
+            print(second_member, end=" ")
+            next_member = first_member + second_member
+            first_member = second_member
+            second_member = next_member
+
+
+Fibonacci(int(input("Enter n value: ")))
