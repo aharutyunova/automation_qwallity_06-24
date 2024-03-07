f2240fb46dd5e40d14320cc19638164b1bed1c3a
hamestdolukhanyan <hamestdolukhanyan@gmail.com>
2024-03-07 16:32:56 +0400

    Fibonachi Hamest version, firs version

---
+++ Hamest_Lesson_3/fibonachi.py
@@ -0,0 +1,20 @@
+'''This is Hamest's fibonachi program
+Program works withe the following logic
+You enther the valid number N of list Elements, and the program return you the fibonachi list with N elements.'''
+
+
+def fibonachi(n):
+    if n <= 0:
+        print("Invalid value")
+    else:
+        a = [0, 1]
+        i = 2
+        while n >= 3:
+            a.insert(i, a[i-1] + a[i-2])
+            n = n-1
+            i = i+1
+        print(a)
+
+
+print("Please enter the number you want to see the fibonachi elements list")
+fibonachi(int(input()))
