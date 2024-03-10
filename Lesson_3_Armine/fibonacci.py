a923523fb9421d4bcbbd6c5d8a92979d367219a6
Anna <an.harutyunova@gmail.com>
2024-03-10 23:44:34 +0400

    Add my comment

--- Lesson_3_Armine/fibonacci.py
+++ Lesson_3_Armine/fibonacci.py
@@ -7,3 +7,17 @@ def fibonacci (x):
     #the second return should print the fibonacci sequence for the first 10 numbers and result should be /
     0, 1, 1, 2, 3, 5, 8, 13, 21, 34
     # this is not a working code, because I do not know coding rules and syntax yet, I just tried to explain)
+
+
+# Anna comment
+    #  It is ok I didn't expect code soution for this task :)
+    #  First condition is correct, what about second condition - why x-1 and x-2? 
+    #  at first we should print 0 and 1 and after it each next number should be the sum of previous 2 numbers If you mean this idea qit is correct :)
+    # Example
+    # x = 0; y =1; z=x+y=1
+    # x=y=1; y=z=1; z=x+y=2
+    # x=y=1;  y=z=2; z = x+y =3
+    # x=y=2;  y=z=3; z = x+y =5
+    # etc
+
+    
