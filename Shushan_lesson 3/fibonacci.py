b704d2deb8e3f0304d5e5d8615df39605ba439f2
Anna <an.harutyunova@gmail.com>
2024-03-10 18:52:54 +0400

    Add my comment

--- Shushan_lesson 3/fibonacci.py
+++ Shushan_lesson 3/fibonacci.py
@@ -12,3 +12,8 @@ def fibonacci(n):
 
 for i in range(11):
     print(i, fibonacci(i))
+
+
+# Anna comment - I didn't expect you write code )) you just need to describe in diagram )
+    # But anyway code logic is correct. Maybe it is a bit longer, but anyway
+    # taking into account that we didn't discuss it everything is very good :)
