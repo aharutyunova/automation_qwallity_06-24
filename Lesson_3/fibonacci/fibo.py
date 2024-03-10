d242686d7488008cd5747e4a25fe17d869923e1a
Anna <an.harutyunova@gmail.com>
2024-03-10 23:12:43 +0400

    Add my comments and flow chart

--- Lesson_3/fibonacci/fibo.py
+++ Lesson_3/fibonacci/fibo.py
@@ -1,16 +1,22 @@
-n1 = 1
-ntemp = 1
-res = 1
+n1 = 0
+n2 = 0
 fiboN = 10
 
-if fiboN <= 0:
-    res = 0
-elif fiboN == 1:
-    res = 1
-else:
-    for i in range(2, fiboN):
-        res += n1
-        n1 = ntemp
-        ntemp = res
+for i in range(fiboN):
+    if i == 0:
+        n1 = 0
+        print(n1)
+    elif i == 1:
+        n2 = 1
+        print(n2)    
+    else:
+        res = n1 + n2
+        n1 = n2
+        n2 = res
+        print(res)
 
-print(res)
+
+
+# I didn't expect code in this stage )) but it's good you try to find out solution via code also
+# In  your solution only last resalt is printed not the sequence
+#  I changed the code a bit to get sequence as a printed reslut
