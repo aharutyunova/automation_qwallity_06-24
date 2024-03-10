23aaebef7bc47d63cc9fbff6f676362867920a3a
Anna <an.harutyunova@gmail.com>
2024-03-10 18:25:24 +0400

    Add my comment

--- Fibonacci folder/Fibonacci.py
+++ Fibonacci folder/Fibonacci.py
@@ -14,3 +14,8 @@ def Fibonacci(n):
 
 Fibonacci(int(input("Enter n value: ")))
 
+# Anna comment - The diagram was enouth as we didn't discuss code yet :) But anyway code is correct :)
+#  In Diagram only one note - you print only first number, after it you assign 
+#    first_member = second_member
+# second_member = next_member  but don't print it.
+# In the code this part is correct.
