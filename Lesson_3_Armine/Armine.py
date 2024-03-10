42a746b87654afab715aa4db345b61fd35c73a25
Armine Barseghyan <barseghyanarmine55@gmail.com>
2024-03-10 22:19:42 +0300

    fibonacci logic, Armine

--- Lesson_3_Armine/Armine.py
+++ Lesson_3_Armine/Armine.py
@@ -1,6 +1,9 @@
-print("It's my first program", end="!/n")
+print("It's my first program", end='!\n')
 print('Armine Barseghyan')
 # I am a QA Engineer, and now I am learning Python
 a = 5
 b = 8
 print(a+b)
+
+message = "Hello world!" 
+print(message)
---
+++ Lesson_3_Armine/fibonacci.py
@@ -0,0 +1,9 @@
+def fibonacci (x): 
+    if x = {0,1} 
+    return x 
+    # the condition above should print 0 and 1
+    return fibonacci (x-1)+(x-2)
+    fibonacci(x) for x in range(10)
+    #the second return should print the fibonacci sequence for the first 10 numbers and result should be /
+    0, 1, 1, 2, 3, 5, 8, 13, 21, 34
+    # this is not a working code, because I do not know coding rules and syntax yet, I just tried to explain)
