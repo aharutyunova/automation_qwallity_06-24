163e8ce0fbc6dc9c8195ccfe54e7f890d26ca01f
Anna <an.harutyunova@gmail.com>
2024-03-10 17:48:35 +0400

    Add my comment

---
+++ Anush_Hayrapetyan/fibonacci.py
@@ -0,0 +1,4 @@
+# Anna Comment
+# Fibonacci sequence is correct, but the logic on the diagram is not very clear
+# Also please push your homeworks on the  your branch, not on the main branch
+# Lets keep main branch only for tasks, and your branches for the solutions
