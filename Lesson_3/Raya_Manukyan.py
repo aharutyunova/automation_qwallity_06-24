2d83169c1a4652ea9576be62044a6109c6d19d97
Raya <100232444+ramanuky@users.noreply.github.com>
2024-03-11 09:47:32 +0400

    Update Raya_Manukyan.py

--- Lesson_3/Raya_Manukyan.py
+++ Lesson_3/Raya_Manukyan.py
@@ -1,13 +1,4 @@
-# Prints 'It's my first program’ and adds '!' at the end on the first line.
-print ("It's my first program!")
-
-# Prints your name and surname on the second line
+print ("It's my first program", end="!/n")
 print ("Raya Manukyan")
-
-# Adds a comment about your background on the third line.
-print ("QA engineer")
-
-# Sum two arbitrary numbers and print the result on the fourth line
-x = 2;
-y = 9;
-print ("Sum two arbitrary numbers =", x + y)
+# QA engineer
+print ("Sum two arbitrary numbers =", 10 + 5)
