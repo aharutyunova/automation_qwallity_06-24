71da68dfbea3a09f4712621233f447fc5f1bd1ae
aharutyunova <55526198+aharutyunova@users.noreply.github.com>
2024-03-13 23:17:09 +0400

    Update lesson_4.py

--- Arman/Lesson_4/lesson_4.py
+++ Arman/Lesson_4/lesson_4.py
@@ -1,103 +1,117 @@
-# 1. Write a Python program to calculate the length of a string.
-my_string = "Here is string for your exercise!"
-length_of_str = len(my_string)
-print(f"Length of a string is {length_of_str}")
+# # 1. Write a Python program to calculate the length of a string.
+# my_string = "Here is string for your exercise!"
+# length_of_str = len(my_string)
+# print(f"Length of a string is {length_of_str}")
 
-'''
-2. Write a Python program to get a string made of the first 2 and the last
-2 chars from a given a string.
-Sample String : 'w3resource'
-Expected Result : 'w3ce'
-Sample String : 'w3'
-Expected Result : 'w3w3'
-'''
+# # Anna - correct
 
-sample_string = "w3resource"
-sample_string_two = "w3"
-first_flast_char = sample_string[0:2] + sample_string[-2:]
-second_flast_char = sample_string_two[0:2] + sample_string_two[-2:]
-print(first_flast_char)
-print(second_flast_char)
+# '''
+# 2. Write a Python program to get a string made of the first 2 and the last
+# 2 chars from a given a string.
+# Sample String : 'w3resource'
+# Expected Result : 'w3ce'
+# Sample String : 'w3'
+# Expected Result : 'w3w3'
+# '''
 
-'''
-3. Write a Python program to add 'ing' at the end of a given string.
-Sample String : 'abc'
-Expected Result : 'abcing'
-'''
+# sample_string = "w3resource"
+# sample_string_two = "w3"
+# first_flast_char = sample_string[0:2] + sample_string[-2:]
+# second_flast_char = sample_string_two[0:2] + sample_string_two[-2:]
+# print(first_flast_char)
+# print(second_flast_char)
 
-chars = "abc"
-add_ing = chars + "ing"
-print(add_ing)
+# # Anna - correct
 
-'''
-4. Write a Python program to replace ‘cut’ word to ‘dog’
-Sample String : 'I have a cut and I love it'
-Expected Result : 'I have a dog and I love it'
-'''
+# '''
+# 3. Write a Python program to add 'ing' at the end of a given string.
+# Sample String : 'abc'
+# Expected Result : 'abcing'
+# '''
 
-cut_str = "I have a cut and I love it"
-dog_str = cut_str.replace("cut", "dog")
-print(dog_str)
+# chars = "abc"
+# add_ing = chars + "ing"
+# print(add_ing)
 
-'''
-# 5. Write a Python program to reverse 123  to 321 in text.
-# Sample String : ‘I have 123 books’
-# Expected Result : 'I have 321 books'
-'''
+# # Anna - correct
+# '''
+# 4. Write a Python program to replace ‘cut’ word to ‘dog’
+# Sample String : 'I have a cut and I love it'
+# Expected Result : 'I have a dog and I love it'
+# '''
 
-books_str = "I have 123 books"
-str_list = books_str.split()
-reverse_number = str_list[2][::-1]
-str_list[2] = reverse_number
-result_str = " ".join(str_list)
-print(result_str)
+# cut_str = "I have a cut and I love it"
+# dog_str = cut_str.replace("cut", "dog")
+# print(dog_str)
 
-'''
-# 6. Replace all occurrence of word five to one.
-# Sample String : "five five was a race horse, two two was one too."
-# Expected Result "one one was a race horse, two two was one too."
-'''
+# # Anna - correct
 
-five_str = "five five was a race horse, two two was one too."
-five_to_one = five_str.replace("five", "one")
-print(five_to_one)
+# '''
+# # 5. Write a Python program to reverse 123  to 321 in text.
+# # Sample String : ‘I have 123 books’
+# # Expected Result : 'I have 321 books'
+# '''
 
-'''
-7. Write a Python program to which will print the triangle perimeter․
-Triangle sides are 3, 4, 7
-Expected Result: 14
-'''
+# books_str = "I have 123 books"
+# str_list = books_str.split()
+# reverse_number = str_list[2][::-1]
+# str_list[2] = reverse_number
+# result_str = " ".join(str_list)
+# print(result_str)
 
-triangle_first_side = int(input("Enter triangle first side: "))
-triangle_second_side = int(input("Enter triangle second side: "))
-triangle_third_side = int(input("Enter triangle third side: "))
-triangle_perimeter = triangle_first_side + triangle_second_side + triangle_third_side
-print(triangle_perimeter)
+# # Anna - correct Interesting solution
+# '''
+# # 6. Replace all occurrence of word five to one.
+# # Sample String : "five five was a race horse, two two was one too."
+# # Expected Result "one one was a race horse, two two was one too."
+# '''
 
-'''
-8. Write a Python program to check whether a specified value is contained in a group of values.
-Test Data :
-3 -> [1, 5, 8, 3] : True
--1 -> [1, 5, 8, 3] : False
-'''
+# five_str = "five five was a race horse, two two was one too."
+# five_to_one = five_str.replace("five", "one")
+# print(five_to_one)
 
-numbers_list = [1, 5, 8, 3]
-ask_for_number = int(input("Enter a number to check existence in list: "))
-if ask_for_number in numbers_list:
-    print(True)
-else:
-    print(False)
+# # Anna - correct
 
-'''
-9. Write a Python program to solve (x + y) * (x + y).
-Test Data : x = 4, y = 3
-Expected Output : (4 + 3) ^ 2) = 49
-'''
+# '''
+# 7. Write a Python program to which will print the triangle perimeter․
+# Triangle sides are 3, 4, 7
+# Expected Result: 14
+# '''
 
-ask_first_number = int(input("Enter first number: "))
-ask_second_number = int(input("Enter second number: "))
-result = (ask_first_number + ask_second_number) * (ask_first_number + ask_second_number)
-print(result)
+# triangle_first_side = int(input("Enter triangle first side: "))
+# triangle_second_side = int(input("Enter triangle second side: "))
+# triangle_third_side = int(input("Enter triangle third side: "))
+# triangle_perimeter = triangle_first_side + triangle_second_side + triangle_third_side
+# print(triangle_perimeter)
+
+# # Anna - correct
+
+# '''
+# 8. Write a Python program to check whether a specified value is contained in a group of values.
+# Test Data :
+# 3 -> [1, 5, 8, 3] : True
+# -1 -> [1, 5, 8, 3] : False
+# '''
+
+# numbers_list = [1, 5, 8, 3]
+# ask_for_number = int(input("Enter a number to check existence in list: "))
+# if ask_for_number in numbers_list:
+#     print(True)
+# else:
+#     print(False)
+# # Anna - correct very good
+# '''
+# 9. Write a Python program to solve (x + y) * (x + y).
+# Test Data : x = 4, y = 3
+# Expected Output : (4 + 3) ^ 2) = 49
+# '''
+
+# ask_first_number = int(input("Enter first number: "))
+# ask_second_number = int(input("Enter second number: "))
+# result = (ask_first_number + ask_second_number) * (ask_first_number + ask_second_number)
+# print(result)
+
+# # Anna - correct
 
 '''
 10. Write a Python program which converts float values to integer, and sum
@@ -111,3 +125,7 @@ second_float_number = float(input("Enter first float number: "))
 sum_float_numbers = int(first_float_number) + int(second_float_number)
 reversed_result_number = str(sum_float_numbers)[::-1]
 print(reversed_result_number)
+
+# Anna - correct
+
+# Very good and clear job Arman jan
