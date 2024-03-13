4f19bed2fd209b884919d7783e75ee6a2cd2f6a1
Anna <an.harutyunova@gmail.com>
2024-03-14 00:37:13 +0400

    Add my comment

--- juli_task4.py
+++ juli_task4.py
@@ -4,6 +4,8 @@
 my_string = "Here is string for your exercise!"
 print("lenght =" ,len(my_string))
 
+# Anna - correct
+
 #2.	Write a Python program to get a string made of the first 2 and the last 2 chars from a given a string. 
 #Expected Result : 'w3ce'
 #Sample String : 'w3'7
@@ -12,12 +14,16 @@ print("lenght =" ,len(my_string))
 str0 = "Barev Dzez"
 print(str0[0] + str0[1] + str0[len(str0)-2] + str0[len(str0)-1])
 
+# Anna - for not hard coding indexes better was write  str0[:2] + str0[-2:]
+
 #3.	Write a Python program to add 'ing' at the end of a given string.
 #Sample String : 'abc'
 #Expected Result : 'abcing'
 
 str1 = "Hello"
 print(str1 + "ing")
+
+# Anna - correct
 
 #4.	Write a Python program to replace ‘cut’ word to ‘dog’
 #Sample String : 'I have a cat and I love it'
@@ -27,6 +33,8 @@ old_str = "I have a cat and I love it"
 new_str = old_str.replace("cat", "dog")
 print(new_str)
 
+# Anna - correct
+
 #5.	Write a Python program to reverse 123  to 321 in text.
 #Sample String : ‘I have 123 books’
 #Expected Result : 'I have 321 books'
@@ -34,6 +42,8 @@ print(new_str)
 str2 = "I have 123 books"
 new_str3 = str2.replace("123",''.join(reversed("123")))
 print(new_str3)
+
+# Anna - correct
 
 # 6.	Replace all occurrence of word five to one.
 # Sample String : "five five was a race horse, two two was one too."
@@ -43,6 +53,8 @@ str3 = "five five was a race horse, two two was one too."
 new_str3 = str3.replace("five","one")
 print(new_str3)
 
+# Anna - correct
+
 # 7.	Write a Python program to which will print the triangle perimeter․
 # Triangle sides are 3, 4, 7
 # Expected Result: 14
@@ -51,6 +63,8 @@ a = 3
 b = 4
 c = 7
 print(a + b + c)
+
+# Anna - correct
 
 # 8.	Write a Python program to check whether a specified value is contained in a group of values.
 # Test Data :
@@ -63,6 +77,8 @@ print(5 in array)
 array2 = [1, 5, 8, 3]
 print(-1 in array2)
 
+# Anna - correct
+
 # 9.	Write a Python program to solve (x + y) * (x + y). 
 # Test Data : x = 4, y = 3
 # Expected Output : (4 + 3) ^ 2) = 49
@@ -70,6 +86,8 @@ print(-1 in array2)
 x = 4
 y = 3
 print((x + y)**2)
+
+# Anna - correct
 
 # 10.Write a Python program which converts float values to integer, and sum of two values, then result print with reversed order.
 # Test Data: x = 2,5 , y = 13.75
@@ -81,6 +99,9 @@ sum = str(int(x) + int(y))
 new_sum = ''.join(reversed(sum))
 print(new_sum)
 
+# Anna - correct
+
+# Good job Jul jan
 
 
 
