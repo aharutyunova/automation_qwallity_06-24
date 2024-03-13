4d7e11287db44fc47bd3790efbfe10ec7e81e8f8
Anna <an.harutyunova@gmail.com>
2024-03-14 00:31:50 +0400

    Add my comment

--- Lesson_4_Armine/Lesson_4_homework.py
+++ Lesson_4_Armine/Lesson_4_homework.py
@@ -4,6 +4,7 @@ my_string = "Here is string for my exercise!"
 y = len(my_string)
 print(y)
 
+# Anna - correct
 
 # 2.Write a Python program to get a string made of the first 2 and the last 2 chars from a given a string. 
 # Sample String : 'w3resource'
@@ -18,6 +19,8 @@ y = 'ce'
 print(x+y)
 print(x*2)
 
+# Anna - for not hard code indexes you could write first_string[:2] + first_string[-2:]
+
 # 3.Write a Python program to add 'ing' at the end of a given string.
 # Sample String : 'abc'
 # Expected Result : 'abcing'
@@ -25,12 +28,16 @@ first_word = 'abc'
 second_word = 'ing'
 print(first_word+second_word)
 
+# Anna - correct
+
 # 4.Write a Python program to replace ‘cat’ word to ‘dog’
 # Sample String : 'I have a cat and I love it
 # Expected Result : 'I have a dog and I love it'
 old_text = 'I have a cat and I love it'
 new_text = old_text.replace('cat', 'dog')
 print(new_text)
+
+# Anna - correct
 
 # 5.Write a Python program to reverse 123  to 321 in text.
 # Sample String : ‘I have 123 books’
@@ -43,12 +50,27 @@ new_sentence = old_sentence.replace('123', '321')
 print(new_sentence)
 # Please, show me another solution to reverse only one word in the sentence))
 
+"""It could be different ways to get valuie from the string based of needs, one of the option is following
+st = 'I have 123 books'
+needed_item = ''
+for item in st:
+    if item.isdigit():
+        needed_item = needed_item+item
+reversed_item = (''.join(reversed(needed_item)))
+print(needed_item)
+print(reversed_item)
+
+"""
+
+
 # 6.Replace all occurrence of word five to one.
 # Sample String : "five five was a race horse, two two was one too."
 # Expected Result "one one was a race horse, two two was one too."
 old_version = "five five was a race horse, two two was one too."
 new_version = old_version.replace('five five', 'one one')
 print(new_version)
+
+# Anna - correct
 
 # 7.Write a Python program to which will print the triangle perimeter․
 # Triangle sides are 3, 4, 7
@@ -58,7 +80,8 @@ b = 4
 c = 7
 print(a+b+c)
 
-         
+# Anna - correct
+    
 # 9.Write a Python program to solve (x + y) * (x + y). 
 # Test Data : x = 4, y = 3
 # Expected Output : (4 + 3) ^ 2) = 49       
@@ -67,6 +90,8 @@ y = 3
 z = (x+y)
 print(z)
 print(z**2)
+
+# Anna - correct
 
 # 10.Write a Python program which converts float values to integer, and sum of two values, then result print with reversed order.
 # Test Data: x = 2,5 , y = 13.75
@@ -78,22 +103,29 @@ print(z)
 z = '15'
 print(''.join(reversed(z)))
 
+
+# Anna - correct
+
 # 8.Write a Python program to check whether a specified value is contained in a group of values.
 # Test Data :
 # 3 -> [1, 5, 8, 3] : True
 # -1 -> [1, 5, 8, 3] : False 
 
 def list_function():
-  list_1 = [1, 5, 8, 3] 
-  h = 3 
-if h in list_1:
-       print(true)
-    else 
-       print(false) 
-    e = -1 
-if e in list_1
-        print(true)
-    else
-         print(false)
+   list_1 = [1, 5, 8, 3] 
+   h = 3 
+   if h in list_1:
+      print(True)
+   else:
+      print(False) 
+   e = -1 
+   if e in list_1:
+      print(True)
+   else:
+      print(False)
+
 
 # I was not able to write a code for this task that would work))
+# Anna - I a bit corrected last funciton
+      
+# Totally good job Armine jan
