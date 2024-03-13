7c698a519f40b91b1ce80d21e70e90b68200b067
Anna <an.harutyunova@gmail.com>
2024-03-14 00:06:33 +0400

    Add my comment

--- Lesson_4/solution_4.py
+++ Lesson_4/solution_4.py
@@ -5,6 +5,7 @@ my_string = ‘Here is string for your exercise!’
 my_string = "Here is string for your exercise!"
 len_of_str = len(my_string)
 print(len_of_str)
+# Anna - correct
 
 '''
 2.	Write a Python program to get a string made of the first 2 and the last 2 chars from a given a string. 
@@ -21,6 +22,8 @@ sample_str_2 = "w3"
 expected_res_2 = sample_str_2[0:2] + sample_str_2[-2:]
 print(expected_res_2)
 
+# Anna - correct
+
 '''
 3.	Write a Python program to add "ing" at the end of a given string.
 Sample String : "abc"
@@ -29,6 +32,8 @@ Expected Result : "abcing"
 sample_str = "abc"
 expected_res = sample_str + "ing"
 print(expected_res)
+
+# Anna - correct
 
 '''
 4.	Write a Python program to replace ‘cut’ word to ‘dog’
@@ -39,6 +44,8 @@ sample_str_4 = "I have a cut and I love it"
 expected_res_4 = sample_str_4.replace("cut", "dog")
 print(expected_res_4)
 
+# Anna - correct
+
 '''
 5.	Write a Python program to reverse 123  to 321 in text.
 Sample String : ‘I have 123 books’
@@ -48,6 +55,8 @@ sample_str_5 = "I have 123 books"
 expected_res_5 = sample_str_5.replace("123", "321")
 print(expected_res_5)
 
+# Anna - correct
+
 '''
 6.	Replace all occurrence of word five to one.
 Sample String : "five five was a race horse, two two was one too."
@@ -56,6 +65,8 @@ Expected Result "one one was a race horse, two two was one too."
 sample_str_6 = "five five was a race horse, two two was one too."
 expected_res_6 = sample_str_6.replace("five", "one")
 print(expected_res_6)
+
+# Anna - correct
 
 '''
 7.	Write a Python program to which will print the triangle perimeter․
@@ -68,6 +79,8 @@ tr_side_c = 7
 tr_perimeter = tr_side_a + tr_side_b + tr_side_c
 print(tr_perimeter)
 
+# Anna - correct
+
 '''
 8.	Write a Python program to check whether a specified value is contained in a group of values.
 Test Data :
@@ -79,6 +92,8 @@ res_8_1 = "True" if 3 in test_data_8 else "False"
 res_8_2 = "True" if -1 in test_data_8 else "False"
 print(res_8_1, res_8_2)
 
+# Anna - correct
+
 '''
 9.	Write a Python program to solve (x + y) * (x + y). 
 Test Data : x = 4, y = 3
@@ -88,6 +103,8 @@ x = 4
 y = 3
 res_9 = (x + y) * (x + y)
 print(f"(({x} + {y}) ^ 2) = {res_9}")
+
+# Anna - correct
 
 '''
 10.Write a Python program which converts float values to integer, and sum of two values, then result print with reversed order.
@@ -100,3 +117,7 @@ res_10 = int(x) + int(y)
 print(res_10)
 reversed_res_10 = int(str(int(x) + int(y))[::-1])
 print(reversed_res_10)
+
+# Anna - correct
+
+# Very good job!!! 
