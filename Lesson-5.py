98f241e12fa23a3f4569fa18e9962e77999f3e0f
Anna <an.harutyunova@gmail.com>
2024-03-17 22:08:58 +0400

    Add my comments

--- Lesson-5.py
+++ Lesson-5.py
@@ -5,21 +5,24 @@ for number in my_list:
      total += number
 print(total)
 
+# Anna you wrote in a biy difficult way, you could write just print(sum(my_list))
+
 
 # 2 Write a Python program to remove duplicates from a list.
 mylist = [1, 2, 3, 3, 4, 5]
 print(set(mylist))
 
-
-
+# Anna - correct, will be better to bring result back to the list  - list(set(mylist))
 
 # 3 Write a Python program which print a specified list after removing the 0th, 4th and 5th elements.
 
 mylist = [1, 2, 3, 3, 4, 5]
+          0  0  1  2  3  4
 del mylist[:1]
 del mylist[3:5]
 print(mylist)
 
+# Anna - correct
 
 # 4 Write a Python program to get the difference between the two lists.
 
@@ -28,12 +31,14 @@ mylist2 = [1, 2, 7]
 difference = tuple(set(mylist) - set(mylist2))
 print(difference)
 
+# Anna - correct, why you don't return result as a list and return as a tuple
 
 # 5 Write a Python program to convert a tuple to a dictionary.-
 mytuple = ('green', 'red', 'black', 'blue')
 dict = dict(enumerate(mytuple))
 print(dict)
 
+# Anna - could be as a solution
 
 # 6 Write a Python program to add a key to a dictionary.
 mydict = {
@@ -45,6 +50,7 @@ mydict["d"] = "4"
 mydict["e"] = "5"
 print(mydict)
 
+# Anna - correct
 
 # 7 Write a Python program to remove a key from a dictionary.
 mydict = {
@@ -55,6 +61,7 @@ mydict = {
 del mydict["c"]
 print(mydict)
 
+# Anna -correct
 
 #  8 Write a Python program to get the maximum and minimum value in a dictionary.
 mydict = {
@@ -67,6 +74,7 @@ max = max(mydict.values())
 print(min)
 print(max)
 
+# Anna correct
 
 # 9 Write a Python program to create a union of sets.
 set1 = {1, 2, 3, 4, 5}
@@ -74,9 +82,14 @@ set2 = {1, "b", "c", "d", "e"}
 myres = set1.union(set2)
 print(myres)
 
+# Anna - correct
 
 # 10 Write a Python program to create a difference of two sets and print output.
 set1 = {1, 2, 3, 4, 5}
 set2 = {1, "b", "c", "d", "e"}
 myres = set1.difference(set2)
 print(myres)
+
+# Anna - Coorect
+
+# Mariam jan very good, hope you understand all solutions. In case some solutions are not clear for you lets discuss them
--- Variables.py
+++ Variables.py
@@ -5,6 +5,8 @@ my_string = 'Here is string for your exercise!'
 string_length = len(my_string)
 print(string_length) 
 
+# Anna - correct
+
 # 2.Write a Python program to get a string made of the first 2 and the last 2 chars from a given a string
 # Sample String : 'w3resource'
 # Expected Result : 'w3ce'
@@ -18,6 +20,8 @@ sample_str = "w3"
 expected_res = sample_str[0:2]+sample_str[-2]
 print(expected_res)
 
+# Anna - for the second case the same combination  sample_str[0:2]+sample_str[-2:]
+
 
 # 3. Write a Python program to add 'ing' at the end of a given string.
 # Sample String : 'abc'
@@ -26,6 +30,9 @@ print(expected_res)
 sample_str = "abc"
 expected_res = sample_str+"ing"
 print(expected_res)
+
+
+# Anna - correct
 
 # 4.	Write a Python program to replace ‘cut’ word to ‘dog’
 # Sample String : 'I have a cut and I love it'
@@ -36,6 +43,8 @@ expected_res = sample_str.replace("cut", "dog")
 print(expected_res)
 
 
+# Anna - correct
+
 # 5.	Write a Python program to reverse 123  to 321 in text.
 # Sample String : ‘I have 123 books’
 # Expected Result : 'I have 321 books'
@@ -45,6 +54,8 @@ expected_res = sample_str.replace("123", "321")
 print(expected_res)
 
 
+# Anna - correct
+
 # 6.	Replace all occurrence of word five to one.
 # Sample String : "five five was a race horse, two two was one too."
 # Expected Result "one one was a race horse, two two was one too."
@@ -53,6 +64,8 @@ sample_str = "five five was a race horse, two two was one too"
 expected_res = sample_str.replace("five", "one")
 print(expected_res)
 
+
+# Anna - correct
 
 # 7.	Write a Python program to which will print the triangle perimeter․
 # Triangle sides are 3, 4, 7
@@ -65,6 +78,8 @@ tr_perimeter = tr_side_1 + tr_side_2 + tr_side_3
 print(tr_perimeter)
 
 
+# Anna - correct
+
 # 8. Write a Python program to check whether a specified value is contained in a group of values.
 # Test Data :
 # 3 -> [1, 5, 8, 3] : True
@@ -76,6 +91,9 @@ res_1 = "True" if 3 in test_data else "False"
 res_2 = "True" if -1 in test_data else "False"
 print(res_1, res_2)
 
+
+# Anna - correct very good
+
 # 9.	Write a Python program to solve (x + y) * (x + y). 
 # Test Data : x = 4, y = 3
 # Expected Output : (4 + 3) ^ 2) = 49
@@ -83,5 +101,10 @@ print(res_1, res_2)
 x = 4
 y = 3
 print((x+y)**2)
+
+
+# Anna - correct
+
+# Mariam jan very good!!!
 
 
