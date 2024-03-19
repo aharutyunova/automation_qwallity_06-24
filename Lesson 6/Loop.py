4e0eeb63a43de09344a13ced2955d64cccb2bd91
IzabellaMnatsakanyan <izabella.mnatsakanyan@jelvix.com>
2024-03-20 02:45:50 +0400

    add_another_solutions

--- Lesson 6/Loop.py
+++ Lesson 6/Loop.py
@@ -42,7 +42,6 @@ def fibonacci(n):
 print(fibonacci(9))
 
 
-
 '''
 2.	Write a Python program that accepts a string and calculate the number of digits and letters.  
 Sample Data: Python 3.2
@@ -63,6 +62,7 @@ for i in sample_data:
         count_dig += 1
 
 print(f"Letters {count_lett}\nDigits {count_dig}")
+#we can also use print command for two lines
 #print("Letters", count_lett)
 #print("Digits", count_dig)
 
@@ -96,6 +96,35 @@ Day: 31
 Season: Summer                                                    
 '''
 
+seasons_list = [
+    {"Month_name": "January", "Month": 1, "Day": 31, "Season": "Winter"},
+    {"Month_name": "February", "Month": 2, "Day": 28, "Season": "Winter"},
+    {"Month_name": "March", "Month": 3, "Day": 31, "Season": "Spring"},
+    {"Month_name": "April", "Month": 4, "Day": 30, "Season": "Spring"},
+    {"Month_name": "May", "Month": 5, "Day": 31, "Season": "Spring"},
+    {"Month_name": "June", "Month": 6, "Day": 30, "Season": "Summer"},
+    {"Month_name": "July", "Month": 7, "Day": 31, "Season": "Summer"},
+    {"Month_name": "August", "Month": 8, "Day": 31, "Season": "Summer"},
+    {"Month_name": "September", "Month": 9, "Day": 30, "Season": "Fall"},
+    {"Month_name": "October", "Month": 10, "Day": 31, "Season": "Fall"},
+    {"Month_name": "November", "Month": 11, "Day": 30, "Season": "Fall"},
+    {"Month_name": "December", "Month": 12, "Day": 31, "Season": "Winter"},
+]
+
+month = int(input("Month: "))
+day = int(input("Day: "))
+
+for i in seasons_list:
+    if i["Month"] == month and 1 <= day <= i["Day"]:
+        print(f"Month: {i['Month_name']}({month})\nDay: {day}\nSeason: {i['Season']}")
+        break
+else:
+    print("Something went wrong!!!")
+
+
+
+# another solution
+
 month = input("Input the month (e.g. January, February etc.): ")
 day = int(input("Input the day: "))
 if month in ('January', 'February', 'March'):
@@ -116,8 +145,8 @@ elif (month == 'December') and (day > 20):
     season = 'winter'
 print("Season is", season)
 
-# will write another solution tomorrow
-'''.;p8uijkjygyhf
+
+'''
 5.	Write a Python program to find the median of few values. 
 Numbers: 15, 26, 28, 33   or   1, 4, 5, 6, 7
 
@@ -129,13 +158,14 @@ Median=6
 1, 2, 3, 4, 5, 6, 8, 9, 
 Median = (4+5)/2 = 4.5
 '''
-val = [1, 2, 3, 4, 5, 6, 8, 9]
+
+val_list = [78, 41, 9, 32, 64]
 
 median = 0
-if len(val) % 2 == 0:
-    before_median = val[:int(len(val)//2)]
-    after_median = val[int(len(val)//2):]
+if len(val_list) % 2 == 0:
+    before_median = val_list[:int(len(val_list)//2)]
+    after_median = val_list[int(len(val_list)//2):]
     median = (before_median[-1] + after_median[0]) / 2
-elif len(val) % 2 != 0:
-    median = val[int(len(val) // 2)]
+elif len(val_list) % 2 != 0:
+    median = val_list[int(len(val_list) // 2)]
 print(median)
