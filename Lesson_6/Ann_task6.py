d41422b69f49cf7e357d53f9c5480aad6c9c24f1
avtanna <annaavtandilyanvolo@gmail.com>
2024-03-20 17:30:50 +0400

    File updated

--- Lesson_6/Ann_task6.py
+++ Lesson_6/Ann_task6.py
@@ -44,13 +44,20 @@ Expected Output:
 *                                                                      
 *****
 '''
+# Solution 1
 height = 7
 for row in range(height):
     if row == height - 1:
         print("*" * (height - 2))
     else:
         print("*")
-
+# Solution 2
+for i in range(11):
+    if i >= 6:
+        print("*", end="")
+    else:
+        print("*")
+print()
 
 '''
 4.	Write a Python program that reads two integers representing a month and day and prints the season for that month and day.  
@@ -61,16 +68,35 @@ Season: Summer
 '''
 
 
-
 '''
 5.	Write a Python program to find the median of few values. 
 Numbers: 15, 26, 28, 33   or   1, 4, 5, 6, 7
 
 Example:
 1, 3, 3, 6, 7, 8, 9
-
 Median=6
 
-1, 2, 3, 4, 5, 6, 8, 9, 
+1, 2, 3, 4, 5, 6, 8, 9,
 Median = (4+5)/2 = 4.5
 '''
+# even numbers
+numbers_list = [1, 2, 3, 4, 5, 6, 8, 9]
+
+if len(numbers_list) % 2 != 0:
+    print(f"Median is: {numbers_list[len(numbers_list) // 2]}")
+else:
+    before_center = int(numbers_list[(len(numbers_list) // 2) - 1])
+    after_center = int(numbers_list[len(numbers_list) // 2])
+    median = (before_center + after_center) / 2
+    print(f"Median is: {median}")
+
+# odd numbers
+numbers_list = [1, 2, 3, 4, 5, 6, 8]
+
+if len(numbers_list) % 2 != 0:
+    print(f"Median is: {numbers_list[len(numbers_list) // 2]}")
+else:
+    before_center = int(numbers_list[(len(numbers_list) // 2) - 1])
+    after_center = int(numbers_list[len(numbers_list) // 2])
+    median = (before_center + after_center) / 2
+    print(f"Median is: {median}")
