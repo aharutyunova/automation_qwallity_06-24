56078d9c5c5f9486d96052aa7c2d99e699ca6d3e
Anna <an.harutyunova@gmail.com>
2024-03-21 00:44:43 +0400

    Add My comments

--- Anush_Les_6.py
+++ Anush_Les_6.py
@@ -8,6 +8,8 @@ while b < 50:
     a = b
     b = a+b
 
+# Anna correct
+
 # Answer: 1, 2, 4, 8, 16, 32
 
 
@@ -26,6 +28,7 @@ for i in x:
 print("Letter:",  num_let)
 print("Digits: ", num_dig)
 
+# Anna correct
 
 # 3.	Write a Python program to print alphabet pattern 'L'
 
@@ -37,6 +40,8 @@ print("*")
 print("*")
 print("*")
 print("*****")
+
+# Anna - will be better use loop for solution
 
 # 4  Write a Python program that reads two integers representing a month and day and prints the season for that month and day.  
 
@@ -66,6 +71,11 @@ elif (month == 'December') or (month == 'January') or (month == 'Febrary'):
 
 print("Season is", season) 
 
+# Anna - the main logic is close to correct, but there some bugs :)
+#  1.as you use season only in if/elif blocks, it is local space, so when you try print season, python gove you error,
+#  you need to define season before if/else, for example in line 52 write season=None
+# 2.per requirement your program should accept month as integerm not by name and print month name
+
 # 5 Write a Python program to find the median of few values.
 numbers1 = [15, 26, 28, 33]
 numbers2 = [1, 4, 5, 6, 7]
@@ -77,7 +87,7 @@ print(y)
 
 
 if x % 2 == 0:
-    print((numbers1[1] + numbers2[2])/2)
+    print((numbers1[1] + numbers1[2])/2)
 else:
     print("nothing")
 
@@ -86,3 +96,7 @@ if y % 2 != 0:
     print(numbers2[2])
 else:
     print("nothing")
+
+# Anna - solution works only in case list len is 5 and 4, as you hardcoded indexes, in case list len is changed, your code willn't work
+    
+# Anna - As it is our first complex task, it is good you find out solutions, even with some mistakes
