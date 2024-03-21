8c66a0081f6a3fea8ea8b1c955814919f984ddd7
Anna <an.harutyunova@gmail.com>
2024-03-21 22:32:42 +0400

    Add lesson 7

---
+++ Lesson 7/task_7.py
@@ -0,0 +1,10 @@
+
+#1. Write a Python function to find the maximum value of given list(write algorithm).
+
+#2.	Write a Python function, which will get few numbers from keyboard, return to sum of them
+
+
+#3.	Create module  with name fib.py where keep function of Fibonacci, in second .py file import and call it.
+
+
+fib()
