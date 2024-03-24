383add3d5972d87bd3019cdbfb037462768db61f
Anna <an.harutyunova@gmail.com>
2024-03-24 23:58:20 +0400

    Add my comment

--- Lesson 7/task_results_7.py
+++ Lesson 7/task_results_7.py
@@ -1,52 +1,55 @@
 
-#1. Write a Python function to find the maximum value of given list(write algorithm).
+# #1. Write a Python function to find the maximum value of given list(write algorithm).
 
-list = [1, 20, 53, 84, 59, 67, 302, 5, 18]
+# list = [1, 20, 53, 84, 59, 67, 302, 5, 18]
 
-def max_value(list):
-    max = list[0]
-    for x in list:
-        if x > max:
-            max = x
-    return max
+# def max_value(list):
+#     max = list[0]
+#     for x in list:
+#         if x > max:
+#             max = x
+#     return max
 
-print(max_value(list))
-
-
-listt = [233.1, 500, 625, 987, 600, 154, 789]
-
-def min_value(listt):
-    min = listt[0]
-    for y in listt:
-        if y < min:
-            min = y
-    return min 
+# print(max_value(list))
 
 
-print(min_value(listt))
+# listt = [233.1, 500, 625, 987, 600, 154, 789]
+
+# def min_value(listt):
+#     min = listt[0]
+#     for y in listt:
+#         if y < min:
+#             min = y
+#     return min 
 
 
-#2.	Write a Python function, which will get few numbers from keyboard, return to sum of them
+# print(min_value(listt))
 
-numbers = [1, 2, 3, 4 , 5 , 6]
+# # Anna -very good
 
+# #2.	Write a Python function, which will get few numbers from keyboard, return to sum of them
 
-def num(numbers):
-    sum = 0
-    for i in numbers:
-        sum = sum + 1
-    return sum
-
-print(num(numbers))
+# numbers = [1, 2, 3, 4 , 5 , 6]
 
 
-def k_num():
-    num = input("Enter any numbers: ").split()
-    num = [int(k) for k in num]
-    tot_sum = sum(num)
-    return tot_sum
+# def num(numbers):
+#     sum = 0
+#     for i in numbers:
+#         sum = sum + 1
+#     return sum
 
-print(k_num())
+# print(num(numbers))
+
+
+# def k_num():
+#     num = input("Enter any numbers: ").split()
+#     num = [int(k) for k in num]
+#     tot_sum = sum(num)
+#     return tot_sum
+
+# print(k_num())
+
+# Anna- correct
 
 
 #3.	Create module  with name fib.py where keep function of Fibonacci, in second .py file import and call it.
@@ -54,3 +57,7 @@ print(k_num())
 import fibonacci_test as fib
 
 print()
+
+# Anna - will be better to write in fibonacci_test.py file only fibonacci function, and in this file call this function and print result
+
+# Totally very good job
