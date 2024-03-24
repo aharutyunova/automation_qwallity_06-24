a82fa9f062eae04fa839aa8f07fd079d57fa22c2
Anna <an.harutyunova@gmail.com>
2024-03-24 23:32:36 +0400

    Add my comment

--- Lesson 7/task1.py
+++ Lesson 7/task1.py
@@ -5,3 +5,6 @@ def Max_from_list(sample_list: list):
    
 sample_list = [1,6,8,2,65,75,21,6,45]
 print(f'Maximum value of given list {sample_list} is: ',Max_from_list(sample_list))
+
+
+# Anna correct
