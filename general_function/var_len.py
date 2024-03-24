6952679491dcdb57709c51ae11478dd592990b20
lianna <tavadyan.lianna89@gmail.com>
2024-03-24 23:08:00 +0400

    general_functions

---
+++ general_function/var_len.py
@@ -0,0 +1,41 @@
+
+#1. Write a Python function to find the maximum value of given list(write algorithm).
+
+def total_price(*args, **kwargs):
+    total = sum(args) + sum(kwargs.values())
+    print(f"Total price is {total}.")
+
+total_price(100, 200, 300, apple=100, banana=200, orange=300)
+
+#2.	Write a Python function, which will get few numbers from keyboard, return to sum of them
+
+def sum_price(x=50, y=50, c=100):
+    total = x + y + c
+    print(total)
+    return total
+
+result = sum_price()
+print("Result:", result)
+    
+
+
+#3.	Create module  with name fib.py where keep function of Fibonacci, in second .py file import and call it.
+
+def Fibonacci(n):
+    if n < 0:
+        print("0")
+    elif n == 0:
+        return 0
+    elif n == 1 or n == 2:
+        return 1
+    else:
+        return Fibonacci(n - 1) + Fibonacci(n - 2)
+    
+limit = 50
+
+print("Fibonacci series up to", limit, ":")
+for i in range(limit):
+    fib_number = Fibonacci(i)
+    if fib_number > limit:
+        break
+    print(fib_number)
