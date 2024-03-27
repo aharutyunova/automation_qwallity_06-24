d84786d85293172aa2179acdad8233b708ba989a
Anna <an.harutyunova@gmail.com>
2024-03-27 22:48:18 +0400

    Add my comment

--- Arman/Lesson_8/solution_8.py
+++ Arman/Lesson_8/solution_8.py
@@ -9,21 +9,26 @@ It's my first file handling! - second line
 Read data from file and print in console. Don't forget and close it, use with block here.
 '''
 
+files = os.getcwd() # Anna example of path
 # files = r"C:\Users\arman.petrosyan\Desktop\Qwallity\Arman\files"
 # file_name = "test_text.txt"
 # full_path = os.path.join(files, file_name)
 # print(full_path)
-#
-#
+
+
 # def write_hello_in_file(input_file):
 #     str_list = ["Hello", "It's my first file handling!"]
 #     if input_file:
 #         with open(input_file, "a") as file:
+
 #             for str_item in str_list:
 #                 file.write(str_item + "\n")
-#
-#
+
+
 # write_hello_in_file(full_path)
+
+# Anna -  the logic and function are correct, only file path is hard coded and 
+# from my side didn't work, so try use some universal path, like getcwd() and build full path based on getcwd()
 
 '''
 2. Write a Python program which will create directory with your name, 
@@ -31,31 +36,33 @@ switch to directory and generate there 26 text files named
 A.txt, B.txt, and so on up to Z.txt.
 '''
 
-# main_file_path = r"C:\Users\arman.petrosyan\Desktop\Qwallity\Arman"
+# main_file_path = os.getcwd()
 
 
 # def alphabetic_files_generator(input_path):
 #     new_directory = "alphabetic_text_files"
 #     file_format = ".txt"
 #     new_directory_path = os.path.join(input_path, new_directory)
-#
+
 #     if not os.path.exists(new_directory_path):
 #         os.mkdir(new_directory_path)
-#
+
 #     for char in range(ord("A"), ord("Z") + 1):
 #         file_path = os.path.join(new_directory_path, chr(char) + file_format)
 #         with open(file_path, "w") as file:
 #             file.write(chr(char))
-#
-#
+
+
 # alphabetic_files_generator(main_file_path)
+
+# Anna - Good solution, again same note for main_file_path, should not be hardcoded
 
 '''
 3. Write a Python program which will open file, add your name, 
 surname and address to the file (keep there as dictionary).
 '''
 
-files = r"C:\Users\arman.petrosyan\Desktop\Qwallity\Arman\files"
+files = r"C:\Qwallity\Automation_02_2024\automation_qwallity_06-24\Arman\files"
 user_file_name = "user_credentials.json"
 user_full_path = os.path.join(files, user_file_name)
 
@@ -76,3 +83,7 @@ def write_user_credential_in_file(input_file):
 
 
 write_user_credential_in_file(user_full_path)
+
+# Anna - we didn't discuss json.dumps method yet, solution and way of writing json is correct :)
+
+# If we skip hardcoded paths all 3 tasks solved in very good way!!!
