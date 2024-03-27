a5b49b5525fb8cb61063c94f8434a92f0d4e0b7f
avtanna <annaavtandilyanvolo@gmail.com>
2024-03-27 18:47:39 +0400

    Updated my homework

--- Lesson_8/homework_8.py
+++ Lesson_8/homework_8.py
@@ -3,12 +3,10 @@
  It's my first file handling! - second line
  Read data from file and print in console. Don't forget and close it, use with block here.
  '''
-
-
 '''
-#Open the file in 'append' mode ('a+'), creating it if it doesn't exist
-with open('my_file.txt', 'a+') as file:
-    # Append the specified text to the file
+# Open file in 'append' mode ('a+'), if not exist than create it
+with open('my_file.txt', 'a+', encoding="utf8") as file:
+    # Append the text to the file
     file.write("Hello\n")
     file.write("It's my first file handling!\n")
 
@@ -18,6 +16,7 @@ with open('my_file.txt', 'r') as file:
     file.read()
     # Print the contents to the console
     print(file.read())
+    # Close
     file.close()
 '''
 
@@ -25,7 +24,24 @@ with open('my_file.txt', 'r') as file:
 2.	Write a Python program which will create directory with your name, 
 switch to directory and generate there 26 text files named A.txt, B.txt, and so on up to Z.txt.
 '''
+'''
+import os
 
+# 1. Define directory name
+directory_name = "Anna_directory"
+
+# 2. Create directory
+os.mkdir(directory_name)
+
+# 3. Switch to created directory
+os.chdir(directory_name)
+
+# 4. Generate there 26 text files named A.txt, B.txt, ..., Z.txt
+for letter in "ABCDEFGHIJKLMNOPQRSTUVWXYZ":
+    file_name = letter + ".txt"
+    with open(file_name, "w", encoding="utf8") as file:
+        file.write(f"{file_name}\n")
+'''
 
 '''
 3.	Write a Python program which will open file, add your name, 
@@ -44,5 +60,5 @@ info = {
     "Address": address
 }
 # Open file and write the dictionary to it
-with open("my_info.txt", "a") as file:
+with open("my_info.txt", "a", encoding="utf8") as file:
     file.write(str(info) + "\n")
