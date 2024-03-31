c76d3f85db5640ad5f4e5580fb13f2302d80d723
Anna <an.harutyunova@gmail.com>
2024-03-31 22:57:14 +0400

    Add my comment

--- Anush_Les_9.py
+++ Anush_Les_9.py
@@ -2,17 +2,16 @@ def calculiator(num1, num2, your_action):
     if your_action == '/' and num2 == 0:
         
         return "Sorry, it is not allowed"
-   
+
     if your_action == '+':
         result = num1 + num2
-
-    if your_action == '-':
+    elif your_action == '-':
         result = num1 - num2
 
-    if your_action == '*':
+    elif your_action == '*':
         result = num1 * num2
 
-    if your_action == '/':
+    elif your_action == '/':
         result = num1 / num2
 
     else:
@@ -26,3 +25,6 @@ num2 = float(input("Enter your second number: "))
 your_action = input("Enter your action: ")
 
 print(calculiator(num1, num2, your_action))
+
+# You should use elif not if because in you example always else result is returned(I already changed to elif)
+# Other logic is correct, you just return always float not int (Per requirement in case result is integer, integer should be returned)
