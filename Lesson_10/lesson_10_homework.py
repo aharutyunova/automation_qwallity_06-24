9d35465bb62ab9f7f6f0a023f8fd094b88734617
Raya Manukyan <ramanuky@gmail.com>
2024-04-04 00:00:03 +0400

    Lesson_9 and lesson 10

--- Lesson_10/lesson_10_homework.py
+++ Lesson_10/lesson_10_homework.py
@@ -3,12 +3,39 @@
 # 1. Write a Python class to reverse a string word by word. Input string : 'hello .py'. 
 # Expected Output : '.py hello'
 
-
+class Reverse:
+    def __init__(self):
+        self.string = input("input string for reversing: ")
+    def reverse_str(self):
+        modif_str = ' '.join(list(reversed(self.string.split())))
+        return modif_str
+x = Reverse()
+print(x.reverse_str())
 
 # 2. Write a Python class which get any string and with methods print_String print this sting in upper case 
 
-
+class Upper:
+    def __init__(self):
+        self.string = input("input string for uppercase: ")
+    def upper_str(self):
+        modif_str = self.string.upper()
+        return modif_str
+x = Upper()
+print(x.upper_str())
 # 3. Write a Python class to generate 26 text files named A.txt, B.txt, and so on up to Z.txt. 
+import os
+class AlphabetFiles:
+    def __init__(self):
+        self.alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    def file_creating(self):
+        os.chdir('Lesson_10')
+        os.mkdir('AZ_Files')
+        os.chdir('AZ_Files')
 
-
-
+        for element in range(0, len(self.alphabet)):
+            file_name = self.alphabet[element] + '.txt'
+            f = open(file_name, 'w+', encoding = 'utf-8')
+            f.close()
+        return 
+x = AlphabetFiles()
+x.file_creating()
--- Lesson_9/workshop_exercise.py
+++ Lesson_9/workshop_exercise.py
@@ -6,3 +6,29 @@ Requirements
 - In all cases result format should be int, for example 2+2 = 4 and 16/4 = 4 but 10/8 should be exact value 1.25
 - Division on 0 is not allowed
 """
+def calc():
+    while True:
+        arg1 = input('input number1: ')
+        arg2 = input('input number2: ')
+        action = input('input action: ')
+        if  arg1.isalpha() or arg2.isalpha() or action not in('+','-', '*', '/'):
+            print('Invalid inputs, try again with valid values', float(arg1), float(arg2))
+            break
+        else:
+            if action == '+':
+                return float(arg1) + float(arg2)
+            elif action == '-':
+                return float(arg1) - float(arg2)
+            elif action == '*':
+                return float(arg1) * float(arg2)
+            else:
+                if arg2 == '0':
+                    print('Division on 0 is not allowed')
+                    break
+                else:
+                    if float(arg1) % float(arg2) == 0:
+                        return int(float(arg1)/float(arg2))
+                    else:
+                        return float(arg1)/float(arg2)
+
+print(calc())
