d1ccb0ea5fd90be807288bc2fcd2f0b6f452bc08
Anna <an.harutyunova@gmail.com>
2024-04-03 22:44:43 +0400

    Add my comment

--- Lesson_10/lesson_10_homework.py
+++ Lesson_10/lesson_10_homework.py
@@ -1,32 +1,36 @@
 # 1. Write a Python class to reverse a string word by word. Input string : 'hello .py'. 
 # Expected Output : '.py hello'
 
-# class My_class:
-#     def __init__(self, my_string):
-#         self.my_string = my_string
-#     def reverse_string(self):
-#         l = self.my_string.split()
-#         l.reverse()
-#         new_string = " "
-#         return new_string.join(l)
+class My_class:
+    def __init__(self, my_string):
+        self.my_string = my_string
+    def reverse_string(self):
+        l = self.my_string.split()
+        l.reverse()
+        new_string = " "
+        return new_string.join(l)
     
 
 # string1 = My_class("hello .py")
 # y = string1.reverse_string()
 # print(y)
 
+# Anna - correct
+
 # 2. Write a Python class which get any string and with methods print_String print this sting in upper case 
 
 
-# class my_class:
-#     def __init__(self,any_string):
-#         self.any_string = any_string
-#     def upper_string(self):
-#         x = self.any_string.upper()
-#         return x
+class my_class:
+    def __init__(self,any_string):
+        self.any_string = any_string
+    def upper_string(self):
+        x = self.any_string.upper()
+        return x
     
 # string1 = my_class("Barev")
 # print(string1.upper_string())
+
+# Anna - correct
 
 # 3. Write a Python class to generate 26 text files named A.txt, B.txt, and so on up to Z.txt. 
 
@@ -36,7 +40,7 @@ import os
 
 
 class Generate_files:
-    os.mkdir("c:/automation_qwallity_06-24/Lesson_10/juli")
+    os.mkdir("c:/automation_qwallity_06-24/Lesson_10/juli") #Anna - path is hardcoded 
     os.chdir("c:/automation_qwallity_06-24/Lesson_10/juli")
     for i in ascii_uppercase:
         with open(i + ".txt", "w+", encoding="utf8") as f:
@@ -44,3 +48,8 @@ class Generate_files:
 
 
 x = Generate_files()
+
+# Anna - Would be better in the Generate_files class to have separate method and under it implement all this functionality
+#  Becauase in this case every time you create object all this functionality will be called
+
+# Anna - Overall good job and correct usage of the class and object
