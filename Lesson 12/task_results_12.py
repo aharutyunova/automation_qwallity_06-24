c4ac694eafff1e6324408e4a7b9c31be9b7badbc
Shushanik Petrosyan <shushan.petrosyan.1998@gmail.com>
2024-04-10 00:11:37 +0400

    add a file

---
+++ Lesson 12/task_results_12.py
@@ -0,0 +1,31 @@
+import logging 
+
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s [%(levelname)s] %(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S', 
+                    filename='my_log.log', 
+                    filemode='w+', 
+                    encoding='utf-8')
+
+"""Create method where you open file,
+write there about what is most difficult and what is most easy for you in python.
+Put your code in "try except finally" block in case if something will go wrong with
+opening/writing file. Handle exception in log file.
+"""
+
+import logging
+
+class writeafile:
+    def write_texts(self):
+        try:
+            with open("texts.txt", "w+") as file:
+                file.write("What's most difficult for me in Python:\n")
+                file.write("OOP section is most difficult part in Python.\n")
+                file.write("What's most easy for me in Python:\n")
+                file.write("Readability of the code due to its simple syntax.\n")
+        except Exception as e:
+            logging.exception(f"An error occurred: {e}")
+        finally:
+            print("File writing process completed.")
+
+writer = writeafile()
+writer.write_texts()
