8bc340b5941857a9f56d39f32006042b09ae1094
Arman Petrosyan <petrosyan.arman.a@gmail.com>
2024-04-10 11:34:38 +0400

    refactor: logging.basingConfig filename change

--- Arman/Lesson_12/try_except_solution_12.py
+++ Arman/Lesson_12/try_except_solution_12.py
@@ -2,7 +2,7 @@ import logging
 
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s [%(levelname)s] %(message)s',
                     datefmt='%Y-%m-%d %H:%M:%S',
-                    filename='my_log.log',
+                    filename='log.log',
                     filemode='w+',
                     encoding='utf-8')
 
