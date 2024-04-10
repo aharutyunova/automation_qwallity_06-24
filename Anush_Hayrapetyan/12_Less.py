24f597bd2b77ec3d8d05026e5354a019f22971a5
Anushhayrapetyan <anush.hayrapetyan@esterox.am>
2024-04-10 17:44:05 +0400

    Less_12

---
+++ Anush_Hayrapetyan/12_Less.py
@@ -0,0 +1,19 @@
+import logging
+
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s [%(levelname)s] %(message)s',
+                    datefmt='%Y-%m  %H:%M:%S',
+                    filename='my_log.log',
+                    filemode='w+',)
+
+try:
+    with open("Anush_log.log", 'w') as f:
+        f.write("Hi Python, you are easy, but I have many difficulties, for example ` 1.OOP, 2.With, Open file. Easy points are 1. Operators, 2.Dictionaries, Arrays, Functions ")
+
+except Exception as te:
+    logging.error(f"Error occurred: {te}")
+
+finally:       
+    print("Done")
+    
+
+# I will send the additional OOP task tomorrow.   
--- Anush_Hayrapetyan/anush.py
+++ Anush_Hayrapetyan/anush.py
@@ -1,8 +1,31 @@
-print("It is my firts program", end='!')
-print("Anush Hayrapetyan")
-# I am a QA and work at Esterox
-print(15+25)
+class Courses:
+    def __init__(self, duration, start_date, student_name, course_title):
+        self.duration = duration
+        self.start_date = start_date
+        self.student_name = student_name
+        self.course_tittle = course_title
+
+    def create_contract(self):
+        pass
+
+    def give_sertificate(self):
+        pass
 
 
-# Anna comment - Everything ok only your Name and surname is not printed on the new line
-# In the first print you should wite end='!\n'
+class Anuthomation(Courses):
+    def learn_p(self):
+        print("Python Agenda")
+
+    def learn_s(self):
+        print("Selenium Agenda")
+
+
+main_course = Anuthomation(
+    duration=5,
+    start_date="15.04",
+    student_name=["Anush, Anna"],
+    course_title="Auth_testing"
+)
+
+
+main_course.create_contract()
