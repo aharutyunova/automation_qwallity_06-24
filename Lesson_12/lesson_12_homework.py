9ded1691d8957583e09b32b3dfcdae06ccc9af83
JulietRev <julietrevazian@gmail.com>
2024-04-10 22:06:02 +0400

    Lesson 12

--- Lesson_12/lesson_12_homework.py
+++ Lesson_12/lesson_12_homework.py
@@ -1,9 +1,11 @@
 import logging 
+import os
+os.chdir("c:/automation_qwallity_06-24/Lesson_12")
 
-logging.basicConfig(level=logging.DEBUG, format='%(asctime)s [%(levelname)s] %(message)s',
+logging.basicConfig(level=logging.ERROR, format='%(asctime)s [%(levelname)s] %(message)s',
                     datefmt='%Y-%m-%d %H:%M:%S', 
                     filename='my_log.log', 
-                    filemode='w+', 
+                    filemode='a+', 
                     encoding='utf-8')
 
 """Create method where you open file,
@@ -11,3 +13,14 @@ write there about what is most difficult and what is most easy for you in python
 Put your code in "try except finally" block in case if something will go wrong with
 opening/writing file. Handle exception in log file.
 """
+
+try:
+    with open("AboutPython.txt", "a+", encoding="utf8" ) as f:
+        f.write('''The Python's syntax is easy.\
+The most difficulty is to remember every lesson for short period.We need to practice a bit more.''')
+except IOError as x:
+    logging.error(x)
+finally:
+    print("The 'try except' is finished")
+
+
