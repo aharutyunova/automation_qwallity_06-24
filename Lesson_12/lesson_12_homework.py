ae460a63117be0721d6c38a79079aee5208aeccc
Raya Manukyan <ramanuky@gmail.com>
2024-04-14 16:02:02 +0400

    lesson12

--- Lesson_12/lesson_12_homework.py
+++ Lesson_12/lesson_12_homework.py
@@ -1,8 +1,13 @@
 import logging 
+import os
+
+path = os.getcwd()
+full_path = os.path.join(path, 'Lesson_12')
+os.chdir(full_path)
 
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s [%(levelname)s] %(message)s',
                     datefmt='%Y-%m-%d %H:%M:%S', 
-                    filename='my_log.log', 
+                    filename='my_log_lesson12.log', 
                     filemode='w+', 
                     encoding='utf-8')
 
@@ -11,3 +16,14 @@ write there about what is most difficult and what is most easy for you in python
 Put your code in "try except finally" block in case if something will go wrong with
 opening/writing file. Handle exception in log file.
 """
+
+def method():
+    try:
+        with open('python.py', 'w+', encoding = 'utf-8') as f:
+            f.writelines(["'The most difficult in python is to not assert it with JS'\n", "'The most easy is syntax'\n"])
+    except FileExistsError as fe:
+        print(str(fe))
+    finally:
+        print("File exists in anyway")
+
+method()
