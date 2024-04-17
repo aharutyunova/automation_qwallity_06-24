a923ea700dc29bed8f488deebaf3ed9e62ecc060
Anna <an.harutyunova@gmail.com>
2024-04-18 01:38:39 +0400

    Add my comments

--- Lesson_14_Liana_Panosyan/solutions.py
+++ Lesson_14_Liana_Panosyan/solutions.py
@@ -5,57 +5,60 @@
 
 # Find out the user id with maximum account.
 
-# import pymysql
+import pymysql
 
-# connection = pymysql.connect(host='pro.freedb.tech',
-#                              user='qwallity',
-#                              password='6YJsZQk&##7J2?e',
-#                              database='qwallitydb')
+connection = pymysql.connect(host='pro.freedb.tech',
+                             user='qwallity',
+                             password='6YJsZQk&##7J2?e',
+                             database='qwallitydb')
 
-# cursor = connection.cursor()
-# query = ("select * from account order by account_balance desc ")
+cursor = connection.cursor()
+query = ("select * from account order by account_balance desc ")
 
-# cursor.execute(query)
-# result = cursor.fetchone()
-# print("The user id with maximum account:", result[0])
-# cursor.close()
-# connection.close()
+cursor.execute(query)
+result = cursor.fetchone()
+print("The user id with maximum account:", result[0])
+cursor.close()
+connection.close()
 
 # Find out the earliest crieted course ID
 
-# import pymysql
+import pymysql
 
-# connection = pymysql.connect(host='pro.freedb.tech',
-#                              user='qwallity',
-#                              password='6YJsZQk&##7J2?e',
-#                              database='qwallitydb')
+connection = pymysql.connect(host='pro.freedb.tech',
+                             user='qwallity',
+                             password='6YJsZQk&##7J2?e',
+                             database='qwallitydb')
 
-# cursor = connection.cursor()
-# query = ("select * from courses order by date_created asc ")
+cursor = connection.cursor()
+query = ("select * from courses order by date_created asc ")
 
-# cursor.execute(query)
-# result = cursor.fetchone()
-# print(" The earliest crieted course ID:", result[0])
-# cursor.close()
-# connection.close()
+cursor.execute(query)
+result = cursor.fetchone()
+print(" The earliest crieted course ID:", result[0])
+cursor.close()
+connection.close()
 
 # Insert new course as author insert your name.
 
-# import pymysql
+import pymysql
 
-# connection = pymysql.connect(host='pro.freedb.tech',
-#                              user='qwallity',
-#                              password='6YJsZQk&##7J2?e',
-#                              database='qwallitydb')
+connection = pymysql.connect(host='pro.freedb.tech',
+                             user='qwallity',
+                             password='6YJsZQk&##7J2?e',
+                             database='qwallitydb')
 
 
-# cursor = connection.cursor()
-# query = """
-#     INSERT INTO courses (title, body, author, date_created, coursetype, price)
-#     VALUES ('New course','Test', 'Liana', DATE_ADD(NOW(), INTERVAL 4 HOUR), 2, 10000)
-# """
+cursor = connection.cursor()
+query = """
+    INSERT INTO courses (title, body, author, date_created, coursetype, price)
+    VALUES ('New course','Test', 'Liana', DATE_ADD(NOW(), INTERVAL 4 HOUR), 2, 10000)
+"""
 
-# cursor.execute(query)
-# connection.commit()
-# cursor.close()
-# connection.close()
+cursor.execute(query)
+connection.commit()
+cursor.close()
+connection.close()
+
+
+# Anna - very good job!!!
