957959a141a37b05d83163dea6856001ee07542d
IzabellaMnatsakanyan <izabella.mnatsakanyan@jelvix.com>
2024-04-22 01:42:34 +0400

    Json_parsing_refactoring.py

--- Lesson 15/import_file_schema_validation.py
+++ Lesson 15/import_file_schema_validation.py
@@ -1,6 +1,6 @@
+'''
 import json
 from marshmallow import Schema, validate, fields, ValidationError
-
 
 class JsonSchema(Schema):
     name = fields.String(required=True)
@@ -23,7 +23,7 @@ try:
 except ValidationError as x:
     print(x.messages)
 else:
-    print("Validated OK")
+    print("Validation is passed")
 
 
 # Anna - General logic and technices are correct, only there is a bit mess, I didn't find one file or method which was final
@@ -36,3 +36,34 @@ number = json_data.get("number")
 print(f"name: {name}\nsurname: {surname}\naddress: {address}\nnumber: {number}")"""
 
 # Anyway despite the comments, the general logic is good enougth :)
+'''
+
+import json
+from marshmallow import Schema, fields, ValidationError
+
+
+class JsonSchema(Schema):
+    name = fields.String(required=True)
+    surname = fields.String(required=True)
+    address = fields.String(required=True)
+    number = fields.Integer()
+
+
+with open("json_file.json", 'r', ) as f:
+    json_data = json.load(f)
+# or json_data = json.loads(json_data_raw)
+
+try:
+    result = JsonSchema().load(json_data)
+    print("Validation passed.")
+    # If validation passes, print the parsed data
+    name = json_data.get("name")
+    surname = json_data.get("surname")
+    address = json_data.get("address")
+    number = json_data.get("number")
+    print(f"Name: {name}\nSurname: {surname}\nAddress: {address}\nNumber: {number}")
+except ValidationError as x:
+    print("Validation failed.")
+    print(x.messages)
+
+# Anna jan is this solution ok?
