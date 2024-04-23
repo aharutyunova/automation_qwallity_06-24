6f4bb87e1243ae42dfc645000c03c92a7265fd8d
MariamG7 <152085808+MariamG7@users.noreply.github.com>
2024-04-24 01:42:46 +0400

    Add files via upload

---
+++ Lesson-15.py
@@ -0,0 +1,24 @@
+'''
+--Task description--
+
+Write a Python program to convert JSON data to Python object.
+
+Create validation scheme in separate file as well.
+
+Create your own JSON data about your name, surname, address, keep it separately,
+import it to you python program file, check if schema validation is pass, parse json
+and print your name, surname and address
+'''
+
+import json
+
+json_data_raw = """{
+        "name": "Mariam",
+        "surname": "Galoyan",
+        "address": "Yerevan",
+        "number": 12345
+}"""
+
+json_data = json.loads(json_data_raw)
+print(json_data)
+print(type(json_data))
