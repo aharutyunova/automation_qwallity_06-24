0fc3b7e5060d09fa8c7d561a8f0d0e37129ba748
Anna <an.harutyunova@gmail.com>
2024-04-25 00:17:18 +0400

    Add my comment

--- reg_ex.py
+++ reg_ex.py
@@ -34,5 +34,17 @@ except BaseException as e:
 finally:
     print(dict(result))
 
+# Anna - The solution is working and data extract and convert way optimal enough
+# For using logging you should have defined basicConfig, like this
+"""
 
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s [%(levelname)s] %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S',
+    filename='my_log.log',
+    filemode='w+',
+    encoding='utf-8'
+)
+"""
 
