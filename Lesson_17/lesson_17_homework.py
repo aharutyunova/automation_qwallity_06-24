7f3819f4bd3b86777f44fc18fe7488e96abe5405
AnnaAvt <annaavtandilyan@gmail.com>
2024-04-28 00:49:46 +0400

    Updated homework 17

--- Lesson_17/lesson_17_homework.py
+++ Lesson_17/lesson_17_homework.py
@@ -4,21 +4,20 @@ import os
 
 def print_temperature(city_name, file_name):
     with open(file_name, 'r') as f:
-        weather_data = json.load(f)  
-    # Extract temperature information
-    temp_kelvin = weather_data['main']['temp']
+        data_file = json.load(f)
+    # Extract city_name, temperature information
+    city_name = data_file['name']
+    temp_kelvin = data_file['main']['temp']
     temp_celsius = temp_kelvin - 273.15
     # We can also import math and rounded_temp_celsius = math.ceil(temp_celsius)
     rounded_temp_celsius = round(temp_celsius)
     print(f"In {city_name} temp is {rounded_temp_celsius}°C")
 
 
-city_name = "Gyumri"
 file_name = "weather.json"
 current_directory = os.getcwd()
 full_file_path = os.path.join(current_directory, file_name)
-print_temperature(city_name, full_file_path)
-
+print_temperature(full_file_path, file_name)
 
 
 # Login to https://qwallity-prod.onrender.com/
