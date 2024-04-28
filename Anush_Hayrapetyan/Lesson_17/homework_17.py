299f2448c7a82198018d86d2f4770f64c1124895
Anna <an.harutyunova@gmail.com>
2024-04-28 21:06:24 +0400

    Add my comment

--- Anush_Hayrapetyan/Lesson_17/homework_17.py
+++ Anush_Hayrapetyan/Lesson_17/homework_17.py
@@ -1,12 +1,16 @@
 import json
     
 with open("json_Anush.json", 'r') as f:
-    data = json.read(f)
+    # data = json.read(f)
+    data = json.load(f)
     
     
 name_of_city = data.get("name")
 temp_kelvin = data["main"].get("temp", 273.15)
-    
+print(temp_kelvin)
 temp_celsius = temp_kelvin - 273.15
     
 print(f"In {name_of_city}, temp is {temp_celsius:.2f}C")
+
+# Anna - json doesn't have read method, you should use json.load(f)
+# Other parts are correct
