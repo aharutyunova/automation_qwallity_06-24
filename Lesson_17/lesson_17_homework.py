bb023745fa1924b0eb84f0aa3283f36c4eb367a6
Raya Manukyan <ramanuky@gmail.com>
2024-04-28 23:09:17 +0400

    Lesson_17

--- Lesson_17/lesson_17_homework.py
+++ Lesson_17/lesson_17_homework.py
@@ -3,3 +3,15 @@
 # In Get Weather write any city name and click  Fet Weather
 # From Network copy Json response and add it in weather.json file
 # Write function which will open that file, parse it and as a result print In [CITY_NAME] temp is [XX]C (temp=273.15)
+import os
+import json
+   
+def getWeather(file):
+    os.chdir("Lesson_17")
+    kelvin = 273.15
+    with open(file, 'r+') as f:
+        result = json.load(f)
+        city = result['name']
+        temp =  int(result['main']['temp'] - kelvin)
+        return f"The temperature in {city} is {temp}°C."
+print(getWeather('weather.json'))
