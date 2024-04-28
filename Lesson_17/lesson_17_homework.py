7eb470082ee162712bfaaf1756e2cc1e08a2dba7
JulietRev <julietrevazian@gmail.com>
2024-04-28 23:08:49 +0400

    Lesson 17

--- Lesson_17/lesson_17_homework.py
+++ Lesson_17/lesson_17_homework.py
@@ -3,3 +3,17 @@
 # In Get Weather write any city name and click  Fet Weather
 # From Network copy Json response and add it in weather.json file
 # Write function which will open that file, parse it and as a result print In [CITY_NAME] temp is [XX]C (temp=273.15)
+
+import json
+import os
+
+os.chdir("c:/automation_qwallity_06-24/Lesson_17")
+def my_func():
+    with open("weather.json", 'r+') as f:
+        result = f.read()
+        parsedJson = json.loads(result)
+        temp = round(parsedJson["main"]["temp"] - 273.15)
+        print(f'In {parsedJson["name"]} temp is {temp}C' )
+
+my_func()
+
