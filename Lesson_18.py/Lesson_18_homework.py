a426a4f4fa7c5661bc54024155e44a23ec7480fb
Anna <an.harutyunova@gmail.com>
2024-04-29 21:57:34 +0400

    Add Lesson 18 Homework

---
+++ Lesson_18.py/Lesson_18_homework.py
@@ -0,0 +1,21 @@
+"""Using python code and APIs
+
+Login to the system with admin_user account                  /login/api
+Add Fundamental Course                                        /add_course/api
+Get Fundamental Course and check your course is added           /courses/fundamental/api
+Update Course by course_id                                      /course/{course_id}/update/
+Delete Course                                                   /courses/course/{course_id}
+
+
+
+At the end you should have following Files
+
+Readme
+login.py
+add_course.py
+get_fundamental_course.py
+update_course.py
+delete_course.py
+config file  
+endpoints file
+main.py """
