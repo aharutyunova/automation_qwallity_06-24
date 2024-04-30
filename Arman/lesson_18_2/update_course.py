9b07d953584c091e2de1f4b475906074adc04d93
Arman Petrosyan <petrosyan.arman.a@gmail.com>
2024-04-30 11:47:47 +0400

    fix: change update_course.py update_course_data function logic

--- Arman/lesson_18_2/update_course.py
+++ Arman/lesson_18_2/update_course.py
@@ -3,7 +3,10 @@ import requests
 from add_course import course_id
 from login import token
 
-UPDATE_COURSE_API_ENDPOINT = f'https://qwallity-prod.onrender.com/course/{course_id}/update'
+UPDATE_COURSE_API_ENDPOINT = f'https://qwallity-prod.onrender.com/course/{course_id}/update/'
+
+# Check which course is going to be updated to the console
+print(UPDATE_COURSE_API_ENDPOINT)
 
 update_course_dict = {
     "title": "Update Python course title",
@@ -16,22 +19,21 @@ def update_course_data(input_data, input_token):
         headers = {"Authorization": f"Bearer {input_token}"}
         response = requests.patch(UPDATE_COURSE_API_ENDPOINT, json=input_data, headers=headers)
         response.raise_for_status()
-
-        # Check if the response contains valid JSON data
-        if response.text:
-            return response.json()
-        else:
-            print("Empty response received.")
-            return None
+        return response.text
     except requests.exceptions.RequestException as error:
         print(f"Error occurred: {error}")
         return None
+
+
+# except requests.exceptions.RequestException as error:
+#     print(f"Error occurred: {error}")
+#     return None
 
 
 # Update course
 updated_course = update_course_data(update_course_dict, token)
 
 if updated_course is not None:
-    print(f"Updated course with id {course_id}: {updated_course}")
+    print(updated_course)
 else:
     print("Failed to update course.")
