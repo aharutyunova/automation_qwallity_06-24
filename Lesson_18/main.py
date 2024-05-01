13bc5b5a055397cbbfe0bb44fb04934d89218d83
Anna <an.harutyunova@gmail.com>
2024-05-02 00:28:37 +0400

    Add my comment

--- Lesson_18/main.py
+++ Lesson_18/main.py
@@ -12,7 +12,9 @@ def main():
     update_fund_course()
     del_fund_courses()
     
-    main()
+main()
+
+
 
 # Anna jan es 2rd tarberakn el toxnem, qani vor chgitei vorn a chisht
 # from login import login, token
@@ -31,6 +33,28 @@ def main():
 #     del_fund_courses(token)
     
 #     main()
+
+
+# An jan you write logic in each separate file correct, but you don't write functions for each action, so your actions are done when you import files,
+# but when you write login(), add_new_course() ..etc as function in main() it is incorrect, as you don't have such functions
+
+# You should write each action as function for example 
+"""
+
+def login():
+    ---
+    return token
+
+def add_new_course()
+    ...
+    return added_id
+...etc
+
+"""
+# After it you could call all this functions in main()
+
+# Endpoint file, readme files are good enough. config file aslo, only body mostly part of test data than configuration part
+
 
 """Using python code and APIs
 
