3490cd3f5e2300f860ccce49627dbdd7f7314e1f
MariamG7 <152085808+MariamG7@users.noreply.github.com>
2024-05-02 16:52:07 +0400

    Add files via upload

---
+++ Lesson-18_Api_endpoints.py
@@ -0,0 +1,14 @@
+endpoints = {"login_api": 'http/login', "add_course": "http/ss"} 
+
+login_api = 'https://qwallity-prod.onrender.com/login/api'
+advanced_courses_api_endpoint = 'https://qwallity-prod.onrender.com/courses/advanced/api'
+fundamental_courses_api_endpoint = 'https://qwallity-prod.onrender.com/courses/fundamental/api'
+
+body_add_course = {
+    "title": "Mariam_title",
+    "body": 'python_body',
+    "coursetype": "1",
+    "author": "python_author",
+    "price": 5000
+}
+    
