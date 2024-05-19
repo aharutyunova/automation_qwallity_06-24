277f46400e0e375e810389bc3add317cfadc408a
Anna <an.harutyunova@gmail.com>
2024-05-20 00:17:44 +0400

    Add my comment

--- Lesson-22.py
+++ Lesson-22.py
@@ -14,4 +14,10 @@ go_button = driver.find_element_by_xpath("//button[@type='submit']")
 go_button.click()
 no_results_found = driver.find_element
 assert "No results found."
-print(error)
+
+# Anna - when you write drivers = [webdriver.Chrome(), webdriver.Firefox()], you open 2 drivers in the same time
+# But it is not so big issue, you have find_element_by_xpath in line 13, but selenium 4 support only
+# driver.find_element(By.XPATH, "//button[@type='submit']") syntax
+# In line 15 you have driver.find_element but don't give which element you want to find, you should give something like
+#  no_results_found = driver.find_element(BY.XPATH, 'any xpath....').text 
+# Then assert "No results found."== no_results_found
