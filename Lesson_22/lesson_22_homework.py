d3d32d4960af1632569fd9aa91285c7260a6a42a
AnnaAvt <annaavtandilyan@gmail.com>
2024-05-19 23:48:42 +0400

    Updated homework 19

--- Lesson_22/lesson_22_homework.py
+++ Lesson_22/lesson_22_homework.py
@@ -54,3 +54,19 @@ for browser in browsers:
     logging.info(f"Starting test on {browser}")
     perform_test(driver)
     logging.info(f"Finished test on {browser}")
+
+
+"""
+Step 1) Open browser
+Step 2) Navigate to python.org
+Step 3) Search "bla bla" text
+Step 4) Click on Go button
+Step 5) Close driver
+Step 6 ) Check Result will be No results found. 
+
+
+Technical solution
+Do the following in all type of browsers(Chrome and Firefox). Make for loop for sequence.
+Handle exceptions
+Use Logging
+"""
