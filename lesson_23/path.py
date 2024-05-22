b69dc24fb854bde3a6047e3de90e2ca1ba7a8446
Anushhayrapetyan <anush.hayrapetyan@esterox.am>
2024-05-22 17:52:43 +0400

    path

---
+++ lesson_23/path.py
@@ -0,0 +1,12 @@
+radio_button = ('//*[@id="radio-btn-example"]/fieldset/label[2]')
+checkbox = ('//*[@id="checkbox-example-div"]/fieldset/label[3]')
+tab = ('//*[@id="opentab"]')
+mul_select_or = ('//*[@id="multiple-select-example"]/option[2]')
+mul_select_peach = ('//*[@id="multiple-select-example"]/option[3]')
+disp_el = ('//*[@id="show-textbox"]')
+hide = ('//*[@id="displayed-text"]')
+swich_alert = ('//*[@id="name"]')
+mouse_hover = ('//*[@id="mouse-hover-example-div"]/div[1]/fieldset/legend')
+pplanguage = ('//*[@id="product"]/tbody/tr[3]/td[2]')
+
+print(len([radio_button, checkbox, tab, mul_select_or, mul_select_peach, disp_el, hide, swich_alert, mouse_hover, pplanguage]))
