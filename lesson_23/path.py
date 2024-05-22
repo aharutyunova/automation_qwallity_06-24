356154232ccfbf99413906880c211d0fec3c38ef
Anna <an.harutyunova@gmail.com>
2024-05-23 00:40:27 +0400

    Add my comment

--- lesson_23/path.py
+++ lesson_23/path.py
@@ -10,3 +10,6 @@ mouse_hover = ('//*[@id="mouse-hover-example-div"]/div[1]/fieldset/legend')
 pplanguage = ('//*[@id="product"]/tbody/tr[3]/td[2]')
 
 print(len([radio_button, checkbox, tab, mul_select_or, mul_select_peach, disp_el, hide, swich_alert, mouse_hover, pplanguage]))
+
+# Anna - in xpaths try avoid use indexes,in case you neet select specific value better to write something like //*[@id="radio-btn-example"]//label[@for='benz']
+# for count elements will be better to open browser and check found elements, or even use loop not hardcode all variables in the list, like in line 12
