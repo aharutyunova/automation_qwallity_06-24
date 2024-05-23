749164d5ab79a869f6ac05dc4b298efc2253f998
Anna <an.harutyunova@gmail.com>
2024-05-23 22:06:27 +0400

    Add Lesson 24 Homework

---
+++ Lesson_24/lesson_24_homework.py
@@ -0,0 +1,7 @@
+"""
+Navigate to "http://www.uitestingplayground.com/"
+
+Go to Visibility, click the Hide button. Check in your program that buttons are hidden.
+Go to Progress Bar, click the Start button, then stop it.  Print Duration.
+Go to Text Input, enter any text, click the button and check if the button text is the same as the entered text.
+"""
