9b94a00179c31534c38eb01281c9f00ca4e28aac
Anna <an.harutyunova@gmail.com>
2024-05-27 01:32:43 +0400

    Add my comment

--- Lesson-24.py
+++ Lesson-24.py
@@ -9,6 +9,7 @@ Go to Text Input, enter any text, click the button and check if the button text 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
+driver = webdriver.Chrome()
 # Progress bar url
 driver.get("http://www.uitestingplayground.com/progressbar")
 
@@ -28,3 +29,10 @@ text_input.send_keys("Test Text")
 
 same_text(text, same_text_button)
 driver.quit()
+
+# Mariam jan you need to initialize driver at the first driver = webdriver.Chrome()
+# when you want find any element write driver.find_element(By.XPATH, "....")  not like you write in line 23 and 26
+
+# In Arman solution same_text() was separate function, in other file, in your example, as you didn't copy that file, this function could not work
+
+# You can see Anna Avtandilyan solution also, try to run it understand how she solve the first task
