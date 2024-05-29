914fd4cde924ca127c0b9f077440454279282c79
Anna <an.harutyunova@gmail.com>
2024-05-29 23:23:02 +0400

    Add my comment

--- Arman/lesson_25/solution_25.py
+++ Arman/lesson_25/solution_25.py
@@ -1,7 +1,8 @@
 import logging
-
+from modules import check_hidden
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
 
 logging.basicConfig(
     level=logging.DEBUG,
@@ -27,86 +28,86 @@ driver.implicitly_wait(5)
 # Navigate to a website
 driver.get('https://www.letskodeit.com/practice')
 
-# try:
-#     # Attempt to find the element with id 'alert_button' and click it
-#     element = driver.find_element(By.XPATH, '//*[@id="alertbtn"]')
-#     element.click()
-#
-#     # Attempt to switch to the alert
-#     alert = driver.switch_to.alert
-#     alert_text = alert.text
-#     print(alert_text)
-#
-#     # Accept the alert (click OK)
-#     alert.accept()
-#     print("Alert accepted!")
-#
+try:
+    # Attempt to find the element with id 'alert_button' and click it
+    element = driver.find_element(By.XPATH, '//*[@id="alertbtn"]')
+    element.click()
+
+    # Attempt to switch to the alert
+    alert = driver.switch_to.alert
+    alert_text = alert.text
+    print(alert_text)
+
+    # Accept the alert (click OK)
+    alert.accept()
+    print("Alert accepted!")
+
 # except NoSuchElementException:
 #     print("Element not found within the given time")
 # except TimeoutException:
 #     print("Alert not found within the given time")
-# except Exception as e:
-#     print(f"An error occurred: {e}")
-#
-# """
-#  4. Get text from the popup and log it
-#  5. Locate the following element, hide it, check element is hidden
-# """
-#
-# # Hide button XPath
-# hide_button = driver.find_element(By.XPATH, "//*[@id='hide-textbox']")
-#
-# # Hide/Show Example input field  XPath
-# hide_input_field_xpath = driver.find_element(By.XPATH, "//*[@id='displayed-text']")
-#
-# # Hide button click
-# hide_button.click()
-#
-# # Get Hide/Show Example input field style attribute
-# hidden_style = hide_input_field_xpath.get_attribute('style')
-#
-# # Check if Hide/Show input field hidden
-# check_hidden(hidden_style)
-#
-# # 7. Scroll and click to the Mouse Hover button and Click on Top option to go to the top of screen
-#
-# # Create an ActionChains object
-# action = ActionChains(driver)
-#
-# hover_button = driver.find_element(By.XPATH, "//*[@id='mousehover']")
-#
-# # Scroll to the "Mouse Hover" button
-# driver.execute_script("arguments[0].scrollIntoView(true);", hover_button)
-#
-# # Click on the Mouse Hover button
-# hover_button.click()
-#
-# # Set an implicit wait of 10 seconds
-# driver.implicitly_wait(5)
-#
-# # Perform mouse hover action on the "Mouse Hover" button
-# action.move_to_element(hover_button).perform()
-#
-# # Find and click on the "Top" option
-# top_option = driver.find_element(By.XPATH, "//div[@class='dropdown-menu']/a[contains(text(), 'Top')]")
-# top_option.click()
-#
-# # 8. Go to the footer and log footer text
-#
-# # Find the footer element
-# footer = driver.find_element(By.XPATH, "//*[@id='page']/div[3]")
-#
-# # Scroll down to the footer element
-# driver.execute_script("arguments[0].scrollIntoView();", footer)
-#
-# # Footer text XPath
-# footer_text = driver.find_element(By.XPATH, "//*[@id='page']/div[3]/div/div/div[1]/div/div[1]/p")
-#
+except Exception as e:
+    print(f"An error occurred: {e}")
+
+"""
+ 4. Get text from the popup and log it
+ 5. Locate the following element, hide it, check element is hidden
+"""
+
+# Hide button XPath
+hide_button = driver.find_element(By.XPATH, "//*[@id='hide-textbox']")
+
+# Hide/Show Example input field  XPath
+hide_input_field_xpath = driver.find_element(By.XPATH, "//*[@id='displayed-text']")
+
+# Hide button click
+hide_button.click()
+
+# Get Hide/Show Example input field style attribute
+hidden_style = hide_input_field_xpath.get_attribute('style')
+
+# Check if Hide/Show input field hidden
+check_hidden(hidden_style)
+
+# 7. Scroll and click to the Mouse Hover button and Click on Top option to go to the top of screen
+
+# Create an ActionChains object
+action = ActionChains(driver)
+
+hover_button = driver.find_element(By.XPATH, "//*[@id='mousehover']")
+
+# Scroll to the "Mouse Hover" button
+driver.execute_script("arguments[0].scrollIntoView(true);", hover_button)
+
+# Click on the Mouse Hover button
+hover_button.click()
+
+# Set an implicit wait of 10 seconds
+driver.implicitly_wait(5)
+
+# Perform mouse hover action on the "Mouse Hover" button
+action.move_to_element(hover_button).perform()
+
+# Find and click on the "Top" option
+top_option = driver.find_element(By.XPATH, "//a[contains(text(), 'Top')]") 
+top_option.click()
+
+# 8. Go to the footer and log footer text
+
+# Find the footer element
+footer = driver.find_element(By.XPATH, "//*[@id='page']/div[3]")
+
+# Scroll down to the footer element
+driver.execute_script("arguments[0].scrollIntoView();", footer)
+
+# Footer text XPath
+footer_text = driver.find_element(By.XPATH, "//*[@id='page']/div[3]/div/div/div[1]/div/div[1]/p")
+
 # Print footer text
-# if footer_text.text:
-#     logging.info(footer_text.text)
-# else:
-#     logging.error("There is no text in the footer")
+if footer_text.text:
+    logging.info(footer_text.text)
+else:
+    logging.error("There is no text in the footer")
 
 """
 9. Click on the Sign In button
@@ -195,3 +196,11 @@ driver.switch_to.window(first_tab)
 
 # Close the browser
 driver.quit()
+
+
+# Anna - I changed xpath in line 92. And related to other xpaths, try to use minimum components and indexes like /a/div[2]/div...etc
+# sign in button not always visible when click on top, so additional move to element is needed for stability
+# If you use implicitly.wait, no need to write it every time, when you one time write driver.implicitly_wait(5) it means for all session , before work with any element, driver will wait 5 seconds
+# Automation result in python.org is 20, the correct xpath is //ul[@class='list-recent-events menu']/li (line 186)
+
+# Generally commands are correct, just need a bit more time and practice
