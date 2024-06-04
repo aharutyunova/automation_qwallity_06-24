4a72a8342d2e210f49d671f101bcd66df8c313a3
Arman Petrosyan <petrosyan.arman.a@gmail.com>
2024-06-04 18:16:27 +0400

    feat: add functions

--- Arman/lesson_27_page_object_model/letskodeit/library/general.py
+++ Arman/lesson_27_page_object_model/letskodeit/library/general.py
@@ -1,10 +1,20 @@
 import logging
 
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s [%(levelname)s] %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S',
+    filename='app.log',
+    filemode='w+',
+    encoding='utf-8'
+)
+
 
 def run_driver(input_driver, url):
     """Function run web browser driver with given URL"""
     try:
         input_driver.get(url)
+        logging.info(f"Web driver successfully ran and opened {url} page")
     except TimeoutError as error:
         logging.error(error)
 
@@ -12,6 +22,22 @@ def run_driver(input_driver, url):
 def close_browser(input_driver):
     """Function close tabs and browser"""
     input_driver.quit()
+    logging.info("Browser closed")
+
+
+def click_alert_popup(driver, alert_xpath):
+    try:
+        # Attempt to find the element with id 'alert_button' and click it
+        alert_xpath.click()
+        # Attempt to switch to the alert
+        alert = driver.switch_to.alert
+        alert_text = alert.text
+        print(alert_text)
+        # Accept the alert (click OK)
+        alert.accept()
+        print("Alert accepted!")
+    except Exception as e:
+        logging.error(f"An error occurred: {e}")
 
 
 def wait(input_driver):
