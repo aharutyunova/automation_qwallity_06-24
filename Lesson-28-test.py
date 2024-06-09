6a845a73e1d0c600ea5735a96db498417fe2a3ea
MariamG7 <152085808+MariamG7@users.noreply.github.com>
2024-06-09 22:57:21 +0400

    Add files via upload

---
+++ Lesson-28-test.py
@@ -0,0 +1,29 @@
+from Helpers.basic_page import BasicHelper
+from Helpers import logging_config
+from Pages.main_login_page import MainLoginPage
+from Test import test_data
+from Test import config
+import logging
+from selenium import webdriver
+
+
+def test():
+    driver = webdriver.Chrome()  # Ensure the path to the ChromeDriver is correctly set
+    logging_config.configure_logging()
+    bh = BasicHelper(driver)
+    main_lgn_page = MainLoginPage(driver)
+
+    try:
+        bh.open_url(config.url)
+        main_lgn_page.fill_and_submit_login_form(test_data.email, test_data.code)
+        logging.info("Successfully logged in!")
+    except Exception as e:
+        logging.error(f"Test failed: {e}")
+    finally:
+        logging.info("Test is complete")
+        driver.quit()
+        logging.info("Closed all opened tabs")
+
+
+if __name__ == '__main__':
+    test()
