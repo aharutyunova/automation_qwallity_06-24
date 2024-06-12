9c00fd049e1fa0e1af7be0e338af5158e431a338
Arman Petrosyan <petrosyan.arman.a@gmail.com>
2024-06-12 11:29:54 +0400

    style: comments change

--- Arman/Lesson_29/pytest_auto_am/tests/test_auto_am.py
+++ Arman/Lesson_29/pytest_auto_am/tests/test_auto_am.py
@@ -6,25 +6,27 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 
+from pytest_auto_am.conftest import driver
+
 
 def test_search_mazda(driver):
-    # Step 1: Navigate to the auto.am website
+    # Navigate to the auto.am website
     driver.get("https://auto.am")
 
-    # Step 2: Find the search box element and clear any existing text
+    # Find the search box element and clear any existing text
     search_box = driver.find_element(By.XPATH, '//*[@id="searchInp-small"]')
     search_box.clear()
 
-    # Step 3: Enter 'Mazda' into the search box
+    # Enter 'Mazda' into the search box
     search_box.send_keys("Mazda")
 
-    # Step 4: Wait for 5 seconds to ensure the search results load
+    # Wait for 5 seconds to ensure the search results load
     time.sleep(5)
 
-    # Step 5: Submit the search by pressing the Enter key
+    # Submit the search by pressing the Enter key
     search_box.send_keys(Keys.RETURN)
 
-    # Step 6: Wait for the search results to load using WebDriverWait
+    # Wait for the search results to load using WebDriverWait
     wait = WebDriverWait(driver, 5)
     try:
         results = wait.until(EC.presence_of_all_elements_located((By.XPATH, '//*[@id="search-result"]')))
@@ -32,8 +34,8 @@ def test_search_mazda(driver):
         print("TimeoutException: No elements found with class name 'car-list-item'")
         raise
 
-    # Step 7: Print the number of results found
+    # Print the number of results found
     print(f"Found {len(results)} results.")
 
-    # Step 8: Check that results are not "Null"
+    # Check that results are not "Null"
     assert len(results) > 0, "No results found for 'Mazda'"
