7f55cdfce700e768374659f1014c83e813248f20
MariamG7 <152085808+MariamG7@users.noreply.github.com>
2024-06-17 01:24:15 +0400

    Add files via upload

---
+++ Lesson-29_test_auto_am.py
@@ -0,0 +1,44 @@
+import pytest 
+from selenium import webdriver
+from auto_am_page import AutoAmPage
+import logging
+
+
+def configure_logging():
+    logging.basicConfig(
+        filename='test_case.log',
+        level=logging.INFO,
+        format='%(asctime)s - %(levelname)s - %(message)s',
+        datefmt='%Y-%m-%d %H:%M:%S'
+    )
+
+
+@pytest.fixture()
+def drive()
+    dr = webdriver.Chrome
+    yield dr
+    dr.quit()
+
+def test_search_kia_results_not_null(driver):
+    auto_page = AutoAmPage(driver)
+    auto_page.go_to_auto_am()
+    auto_page.search_for_brand('Kia')
+    results = auto_page.get_search_results()
+    assert results is not None, "Search results for Kia are null"
+
+    class AutoAmPage:
+      def __init__(self, driver):
+        self.driver = driver
+        self.url = "https://auto.am"
+
+      def go_to_auto_am(self):
+        self.driver.get(self.url)
+
+    def search_for_brand(self, brand):
+        search_box = self.driver.find_element_by_id("main-search-input")
+        search_box.clear()
+        search_box.send_keys(brand)
+        search_box.submit()
+
+    def get_search_results(self):
+       pass
